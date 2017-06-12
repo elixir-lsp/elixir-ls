c271122b3b8b99d7484f198ca5464157ef555048
Jake Becker <jake@jakebecker.org>
2017-06-12 13:36:20 -0700

    Fix launch script when Elixir is installed with "asdf"
    Turns out that the directory structure of "asdf" is similar enough to Elixir that the previous condition was inadequate.

--- bin/exscript.sh
+++ bin/exscript.sh
@@ -17,7 +17,7 @@ REL_DIR="$(dirname $(readlink_f $(which elixir)))/../lib"
 # If directory does not exist, it could be because the `elixir` in the PATH runs a script which runs
 # elixir. (Installations via the "asdf" tool work this way.) We run Elixir and ask it its path as a
 # fallback. This is slower.
-if [ -d "$REL_DIR" ]; then 
+if [ -d "$REL_DIR/elixir" ]; then 
   ELIXIR_LIB=$(cd "$REL_DIR"; pwd)
 else
   ELIXIR_LIB=`elixir -e "IO.puts Path.expand(Path.join(Application.app_dir(:elixir), '..'))"`
