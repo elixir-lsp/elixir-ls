c75e191f5b7b19fdb5c4d5c3eb98701cbb652e4c
Jake Becker <jake@jakebecker.org>
2017-06-23 11:07:59 -0700

    Fix handling of Windows paths with non-default drive letter
    I had thought `Path.absname` would turn `/d:/path` into `d:/path` but it actually substitutes whatever the default drive letter is. This changes it to strip the leading slash instead of using `Path.absname`.

--- apps/language_server/lib/language_server/definition.ex
+++ apps/language_server/lib/language_server/definition.ex
@@ -3,12 +3,14 @@ defmodule ElixirLS.LanguageServer.Definition do
   Go-to-definition provider utilizing Elixir Sense
   """
 
+  alias ElixirLS.LanguageServer.SourceFile
+
   def definition(text, line, character) do
     case ElixirSense.definition(text, line + 1, character + 1) do
       {"non_existing", nil} -> []
-      {file, line} -> 
+      {file, line} ->
         line = line || 0
-        uri = "file://" <> file
+        uri = SourceFile.path_to_uri(file)
         %{"uri" => uri, "range" => %{"start" => %{"line" => line - 1, "character" => 0},
           "end" => %{"line" => line - 1, "character" => 0}}}
     end
--- apps/language_server/lib/language_server/source_file.ex
+++ apps/language_server/lib/language_server/source_file.ex
@@ -1,8 +1,8 @@
 defmodule ElixirLS.LanguageServer.SourceFile do
   defstruct [
-    text: nil, 
-    path: nil, 
-    version: nil, 
+    text: nil,
+    path: nil,
+    version: nil,
     changed_since_compile?: true
   ]
 
@@ -23,8 +23,18 @@ defmodule ElixirLS.LanguageServer.SourceFile do
   Returns path from URI in a way that handles windows file:///c%3A/... URLs correctly
   """
   def path_from_uri(uri) do
-    URI.parse(uri).path
-    |> URI.decode
-    |> Path.absname
+    uri_path = URI.decode(URI.parse(uri).path)
+    case :os.type do
+      {:win32, _} -> String.trim_leading(uri_path, "/")
+      _ -> uri_path
+    end
+  end
+
+  def path_to_uri(path) do
+    uri_path = URI.encode(path)
+    case :os.type do
+      {:win32, _} -> "file:///" <> uri_path
+      _ -> "file://" <> uri_path
+    end
   end
 end
