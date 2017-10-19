df6d0cf0056eabfdf80b3c66ba281a040d5d8cbe
Jake Becker <jake@jakebecker.org>
2017-10-19 12:33:54 -0700

    Bump patch version

--- apps/language_server/mix.exs
+++ apps/language_server/mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirLS.LanguageServer.Mixfile do
 
   def project do
     [app: :language_server,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: ">= 1.6.0-dev",
      build_path: "../../_build",
      config_path: "config/config.exs",
