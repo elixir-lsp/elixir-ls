604b93c3a598144408c5be2290eb264bba65da9e
Jake Becker <jake@jakebecker.org>
2017-10-24 13:29:07 -0700

    Configure logger in language server mix task

--- apps/language_server/lib/mix.tasks.elixir_ls.language_server.ex
+++ apps/language_server/lib/mix.tasks.elixir_ls.language_server.ex
@@ -4,8 +4,24 @@ defmodule Mix.Tasks.ElixirLs.LanguageServer do
 
   def run(_args) do
     WireProtocol.intercept_output(&JsonRpc.print/1, &JsonRpc.print_err/1)
+    configure_logger()
     Application.ensure_all_started(:language_server, :permanent)
     Mix.shell(ElixirLS.LanguageServer.MixShell)
     WireProtocol.stream_packets(&JsonRpc.receive_packet/1)
+  end
+
+  defp configure_logger do
+    use Mix.Config
+
+    Mix.Config.persist(logger: [
+      handle_otp_reports: true,
+      handle_sasl_reports: true,
+      level: :warn,
+      backends: [{ElixirLS.LanguageServer.LoggerBackend, :lsp_logger_backend}]
+    ])
+
+    logger = Process.whereis(Logger)
+    if logger, do: Logger.App.stop()
+    Logger.App.start()
   end
 end
