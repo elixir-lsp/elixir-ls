f9023bb27fb0398a99100118b60addc54af0e19b
Jake Becker <jake@jakebecker.org>
2017-10-24 12:00:10 -0700

    Bump version numbers

--- apps/debugger/mix.exs
+++ apps/debugger/mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirLS.Debugger.Mixfile do
 
   def project do
     [app: :debugger,
-     version: "0.2.0",
+     version: "0.2.1",
      build_path: "../../_build",
      config_path: "config/config.exs",
      deps_path: "../../deps",
--- apps/elixir_ls_utils/mix.exs
+++ apps/elixir_ls_utils/mix.exs
@@ -3,7 +3,7 @@ defmodule ElixirLS.Utils.Mixfile do
 
   def project do
     [app: :elixir_ls_utils,
-     version: "0.2.0",
+     version: "0.2.1",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
