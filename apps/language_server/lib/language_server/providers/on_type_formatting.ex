d43cb073d71e96930af0ea0688d3af21c1cdf8b8
Jake Becker <jake@jakebecker.org>
2018-03-08 15:54:37 -0800

    Improve automatic 'end' insertion

--- apps/language_server/lib/language_server/providers/on_type_formatting.ex
+++ apps/language_server/lib/language_server/providers/on_type_formatting.ex
@@ -4,7 +4,7 @@ defmodule ElixirLS.LanguageServer.Providers.OnTypeFormatting do
 
   We insert the "end" if terminators are incorrect and adding "end" would fix them, or if the next
   non-blank line is indented less than the line where the user hit "enter" or is indented the same
-  but does not start with "end"
+  but does not start with "end" or "else"
   """
 
   alias ElixirLS.LanguageServer.SourceFile
@@ -13,27 +13,33 @@ defmodule ElixirLS.LanguageServer.Providers.OnTypeFormatting do
   def format(source_file, line, character, "\n", _options) do
     lines = SourceFile.lines(source_file)
     prev_line = Enum.at(lines, line - 1)
-    prev_tokens = String.split(prev_line)
+
+    prev_tokens = tokens(prev_line)
 
     if Enum.at(prev_tokens, -1) in ["do", "->"] and not terminators_correct?(source_file.text) do
-      cur_line_blank? = blank?(Enum.at(lines, line))
       prev_indentation = indentation(prev_line)
       prev_indentation_length = String.length(prev_indentation)
 
-      # The contents and indentation of the next line help us guess whether to insert an "end"
-      next_line = Enum.find(Enum.slice(lines, (line + 1)..-1), "", &(!blank?(&1)))
-      next_tokens = String.split(next_line)
-      next_indentation_length = String.length(indentation(next_line))
+      # Use contents and indentation of the next line to help us guess whether to insert an "end"
+      indentation_suggests_edit? =
+        if Enum.at(prev_tokens, -1) == "do" or "fn" in prev_tokens do
+          next_line = Enum.find(Enum.slice(lines, (line + 1)..-1), "", &(!blank?(&1)))
+          next_tokens = tokens(next_line)
+          next_indentation_length = String.length(indentation(next_line))
 
-      indentation_suggests_edit =
-        next_indentation_length < prev_indentation_length or
-          (next_indentation_length == prev_indentation_length and Enum.at(next_tokens, 0) != "end")
+          next_indentation_length < prev_indentation_length or
+            (next_indentation_length == prev_indentation_length and
+               Enum.at(next_tokens, 0) not in ["end", "else"])
+        else
+          false
+        end
 
       # Apply the proposed change to the current text so we can see if it fixes terminators
-      {range, new_text} = insert_end_edit(prev_indentation, line, character, cur_line_blank?)
+      insert_on_next_line? = blank?(Enum.at(lines, line)) and line != Enum.count(lines) - 1
+      {range, new_text} = insert_end_edit(prev_indentation, line, character, insert_on_next_line?)
       edited_text = SourceFile.apply_edit(source_file.text, range, new_text)
 
-      if indentation_suggests_edit or terminators_correct?(edited_text) do
+      if indentation_suggests_edit? or terminators_correct?(edited_text) do
         {:ok, [%{"range" => range, "newText" => new_text}]}
       else
         {:ok, nil}
@@ -49,6 +55,10 @@ defmodule ElixirLS.LanguageServer.Providers.OnTypeFormatting do
     match?({:ok, _}, Code.string_to_quoted(text))
   end
 
+  defp tokens(line) do
+    Regex.scan(~r/(?:->)|(?:\w+)/, line) |> List.flatten()
+  end
+
   defp indentation(line) do
     [indentation] = Regex.run(Regex.recompile!(~r/^\s*/), line)
     indentation
@@ -58,8 +68,8 @@ defmodule ElixirLS.LanguageServer.Providers.OnTypeFormatting do
 
   # In VS Code, currently, the cursor jumps strangely if the current line is blank and we try to
   # insert a newline at the current position, so unfortunately, we have to check for that.
-  defp insert_end_edit(indentation, line, character, cur_line_blank?) do
-    if cur_line_blank? do
+  defp insert_end_edit(indentation, line, character, insert_on_next_line?) do
+    if insert_on_next_line? do
       {range(line + 1, 0, line + 1, 0), "#{indentation}end\n"}
     else
       {range(line, character, line, character), "\n#{indentation}end"}
