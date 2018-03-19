aed326950f838adffb0a4ec8bb1a5e664dbef588
Jake Becker <jake@jakebecker.org>
2018-03-19 13:03:40 -0700

    Bump version numbers

--- apps/debugger/mix.exs
+++ apps/debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :debugger,
-      version: "0.2.17",
+      version: "0.2.18",
       build_path: "../../_build",
       config_path: "config/config.exs",
       deps_path: "../../deps",
