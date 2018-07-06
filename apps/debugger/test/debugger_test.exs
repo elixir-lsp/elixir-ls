d7d08ec996bfd24b0b96226a0b40abc9a06c9968
Jake Becker <jake@jakebecker.org>
2018-07-06 13:28:05 -0700

    Remove check for variable name in test because it depends on Elixir version

--- apps/debugger/test/debugger_test.exs
+++ apps/debugger/test/debugger_test.exs
@@ -112,7 +112,7 @@ defmodule ElixirLS.Debugger.ServerTest do
       assert_receive response(_, 8, "variables", %{
                        "variables" => [
                          %{
-                           "name" => "_x@1",
+                           "name" => _,
                            "type" => "integer",
                            "value" => "2",
                            "variablesReference" => 0
