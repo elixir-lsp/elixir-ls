5c3bf8114b7816448f1a790afd173f1cdac24afb
Jake Becker <jake@jakebecker.org>
2018-07-13 13:32:20 -0700

    Bump all version numbers up to 0.2.21 to be in sync with VS Code plugin

--- apps/debugger/mix.exs
+++ apps/debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :debugger,
-      version: "0.2.19",
+      version: "0.2.21",
       build_path: "../../_build",
       config_path: "config/config.exs",
       deps_path: "../../deps",
