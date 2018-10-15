5dd552f7022034717ff05b4e465a650fc0f6755d
Jake Becker <jake@jakebecker.org>
2018-10-15 16:40:22 -0700

    Bump version numbers

--- apps/language_server/mix.exs
+++ apps/language_server/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.LanguageServer.Mixfile do
   def project do
     [
       app: :language_server,
-      version: "0.2.23",
+      version: "0.2.24",
       elixir: ">= 1.6.5",
       build_path: "../../_build",
       config_path: "config/config.exs",
