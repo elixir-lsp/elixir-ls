ed758f95b833a6f13fb3887b0d01fa44a25cd6cc
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2019-06-22 23:49:43 -0300

    chore: tidy up deps
    mix.lock had many unused deps. Updated all of them to most recent
    ones.
    
    Also cleaned up default generated comments on mix.exs.
    
    Left a REAL important one about NOT starting applications in
    elixir_ls_utils application. Took me a while to figure out WHAT was
    causing the server to stop working.

--- apps/elixir_ls_utils/mix.exs
+++ apps/elixir_ls_utils/mix.exs
@@ -19,28 +19,15 @@ defmodule ElixirLS.Utils.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
+    # We must NOT start ANY applications as this is taken care in code.
     [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:my_app, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:jason, "~> 1.0"}, {:mix_task_archive_deps, github: "JakeBecker/mix_task_archive_deps"}]
+    [
+      {:jason, "~> 1.1"},
+      {:mix_task_archive_deps, github: "JakeBecker/mix_task_archive_deps"}
+    ]
   end
 end
--- apps/language_server/mix.exs
+++ apps/language_server/mix.exs
@@ -18,30 +18,17 @@ defmodule ElixirLS.LanguageServer.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [mod: {ElixirLS.LanguageServer, []}, extra_applications: [:mix, :logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:elixir_ls_utils, in_umbrella: true},
       {:elixir_sense, github: "elixir-lsp/elixir_sense"},
       {:forms, "~> 0.0.1"},
       {:erl2ex, github: "dazuma/erl2ex"},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.6", runtime: false}
     ]
   end
 end
