6cc58bd81d440b4043af904af48277ab71e75936
Victor Oliveira Nascimento <376386+victorolinasc@users.noreply.github.com>
2019-06-24 04:02:02 -0300

    chore: tidy up deps (#33)
    mix.lock had many unused deps. Updated all of them to most recent
    ones.
    
    Also cleaned up default generated comments on mix.exs.
    
    Left a REAL important one about NOT starting applications in
    elixir_ls_utils application. Took me a while to figure out WHAT was
    causing the server to stop working.

--- apps/debugger/mix.exs
+++ apps/debugger/mix.exs
@@ -18,27 +18,10 @@ defmodule ElixirLS.Debugger.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [mod: {ElixirLS.Debugger, []}, extra_applications: [:mix, :logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:my_app, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:elixir_sense, github: "elixir-lsp/elixir_sense"},
