e904a0df8c789eb0ab9f3090968f691ebe419f29
Jonas Trantina <jonas.trantina@altworx.com>
2019-06-26 09:32:05 +0200

    Requesting didSave notification in server capabilities
    Since the server is using didSave notification, it needs to specifically ask for
    it in the server capabilities. In VS Code it works fine without this, because
    the editor sends the notification regardless of what server advertises. However,
    other editors, such as Emacs with lsp-mode, break without this config.

--- apps/language_server/lib/language_server/server.ex
+++ apps/language_server/lib/language_server/server.ex
@@ -447,7 +447,11 @@ defmodule ElixirLS.LanguageServer.Server do
   defp server_capabilities do
     %{
       "macroExpansion" => true,
-      "textDocumentSync" => 2,
+      "textDocumentSync" => %{
+        "change" => 2,
+        "openClose" => true,
+        "save" => true
+      },
       "hoverProvider" => true,
       "completionProvider" => %{"triggerCharacters" => Completion.trigger_characters()},
       "definitionProvider" => true,
