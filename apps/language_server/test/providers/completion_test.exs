34ebffac16b1cf0f2d291b65b29a0cbf717cc57f
Lukasz Samson <lukaszsamson@gmail.com>
2019-12-04 22:34:31 +0100

    provide completions for protocol functions

--- apps/language_server/test/providers/completion_test.exs
+++ apps/language_server/test/providers/completion_test.exs
@@ -55,4 +55,30 @@ defmodule ElixirLS.LanguageServer.Providers.CompletionTest do
       assert(Enum.any?(items, fn %{"label" => label} -> label == lfn end))
     end
   end
+
+  test "provides completions for protocol functions" do
+    text = """
+    defimpl Enumerable, for: MyModule do
+
+    #^
+    end
+    """
+
+    {line, char} = {1, 1}
+    TestUtils.assert_has_cursor_char(text, line, char)
+    {:ok, %{"items" => items}} = Completion.completion(text, line, char, true)
+
+    completions =
+      items
+      |> Enum.filter(&(&1["detail"] == "Enumerable protocol function"))
+      |> Enum.map(& &1["label"])
+      |> Enum.sort()
+
+    assert completions == [
+             "def count(enumerable)",
+             "def member?(enumerable,element)",
+             "def reduce(enumerable,acc,fun)",
+             "def slice(enumerable)"
+           ]
+  end
 end
