0a64aeb3ad2c59aea093a8a13bae2ece8a31cca0
Lukasz Samson <lukaszsamson@gmail.com>
2020-02-13 13:58:39 +0100

    don't assert on exact uris

--- apps/language_server/test/providers/workspace_symbols_test.exs
+++ apps/language_server/test/providers/workspace_symbols_test.exs
@@ -49,12 +49,13 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                 kind: 2,
                 location: %{
                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                  uri:
-                    "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                  uri: uri
                 },
                 name: "ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols"
               }
-            ]} == WorkspaceSymbols.symbols("ElixirLS.LanguageServer.Fixtures.")
+            ]} = WorkspaceSymbols.symbols("ElixirLS.LanguageServer.Fixtures.")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
 
     assert {:ok,
             [
@@ -62,12 +63,13 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                 kind: 2,
                 location: %{
                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                  uri:
-                    "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                  uri: uri
                 },
                 name: "ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols"
               }
-            ]} == WorkspaceSymbols.symbols("work")
+            ]} = WorkspaceSymbols.symbols("work")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
   end
 
   test "returns functions" do
@@ -78,58 +80,49 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                  kind: 12,
                  location: %{
                    range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   uri: uri
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.module_info/1"
                },
                %{
                  kind: 12,
                  location: %{
-                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}}
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.module_info/0"
                },
                %{
                  kind: 12,
                  location: %{
-                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}}
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.behaviour_info/1"
                },
                %{
                  kind: 12,
                  location: %{
-                   range: %{end: %{character: 0, line: 3}, start: %{character: 0, line: 2}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 3}, start: %{character: 0, line: 2}}
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_macro/1"
                },
                %{
                  kind: 12,
                  location: %{
-                   range: %{end: %{character: 0, line: 2}, start: %{character: 0, line: 1}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 2}, start: %{character: 0, line: 1}}
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_function/1"
                },
                %{
                  kind: 12,
                  location: %{
-                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 1}, start: %{character: 0, line: 0}}
                  },
                  name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.__info__/1"
                }
              ]
-           } == WorkspaceSymbols.symbols("f ElixirLS.LanguageServer.Fixtures.")
+           } = WorkspaceSymbols.symbols("f ElixirLS.LanguageServer.Fixtures.")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
 
     assert {:ok,
             [
@@ -137,12 +130,13 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                 kind: 12,
                 location: %{
                   range: %{end: %{character: 0, line: 2}, start: %{character: 0, line: 1}},
-                  uri:
-                    "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                  uri: uri
                 },
                 name: "f ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_function/1"
               }
-            ]} == WorkspaceSymbols.symbols("f fun")
+            ]} = WorkspaceSymbols.symbols("f fun")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
   end
 
   test "returns types" do
@@ -153,22 +147,21 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                  kind: 5,
                  location: %{
                    range: %{end: %{character: 0, line: 8}, start: %{character: 0, line: 7}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   uri: uri
                  },
                  name: "t ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_type/0"
                },
                %{
                  kind: 5,
                  location: %{
-                   range: %{end: %{character: 0, line: 9}, start: %{character: 0, line: 8}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 9}, start: %{character: 0, line: 8}}
                  },
                  name: "t ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_opaque_type/0"
                }
              ]
-           } == WorkspaceSymbols.symbols("t ElixirLS.LanguageServer.Fixtures.")
+           } = WorkspaceSymbols.symbols("t ElixirLS.LanguageServer.Fixtures.")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
 
     assert {
              :ok,
@@ -177,13 +170,14 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                  kind: 5,
                  location: %{
                    range: %{end: %{character: 0, line: 9}, start: %{character: 0, line: 8}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   uri: uri
                  },
                  name: "t ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_opaque_type/0"
                }
              ]
-           } == WorkspaceSymbols.symbols("t opa")
+           } = WorkspaceSymbols.symbols("t opa")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
   end
 
   test "returns callbacks" do
@@ -194,22 +188,21 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                  kind: 24,
                  location: %{
                    range: %{end: %{character: 0, line: 5}, start: %{character: 0, line: 4}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   uri: uri
                  },
                  name: "c ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_callback/1"
                },
                %{
                  kind: 24,
                  location: %{
-                   range: %{end: %{character: 0, line: 6}, start: %{character: 0, line: 5}},
-                   uri:
-                     "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                   range: %{end: %{character: 0, line: 6}, start: %{character: 0, line: 5}}
                  },
                  name: "c ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_macrocallback/1"
                }
              ]
-           } == WorkspaceSymbols.symbols("c ElixirLS.LanguageServer.Fixtures.")
+           } = WorkspaceSymbols.symbols("c ElixirLS.LanguageServer.Fixtures.")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
 
     assert {:ok,
             [
@@ -217,12 +210,13 @@ defmodule ElixirLS.LanguageServer.Providers.WorkspaceSymbolsTest do
                 kind: 24,
                 location: %{
                   range: %{end: %{character: 0, line: 6}, start: %{character: 0, line: 5}},
-                  uri:
-                    "file:///Users/lukaszsamson/vscode-elixir-ls/elixir-ls/apps/language_server/test/support/fixtures/workspace_symbols.ex"
+                  uri: uri
                 },
                 name: "c ElixirLS.LanguageServer.Fixtures.WorkspaceSymbols.some_macrocallback/1"
               }
-            ]} == WorkspaceSymbols.symbols("c macr")
+            ]} = WorkspaceSymbols.symbols("c macr")
+
+    assert uri |> String.ends_with?("test/support/fixtures/workspace_symbols.ex")
   end
 
   defp wait_until_indexed(pid) do
