elixir-lsp/elixir-ls

commit b1415ce8611657826083e6de7b793389b7edc549

Remove elixir 1.6 code as 1.7 is required (#93)
* remove elixir 1.6 code as 1.7 is required

replace deprecated Code.load_file/1 with Code.require_file/1

* remove not used param

* fix dialyzer error: The test binary() == nil can never evaluate to 'true'

apps/debugger/lib/debugger/server.ex
@@ -435,7 +435,7 @@ defmodule ElixirLS.Debugger.Server do
     mixfile = Path.absname(System.get_env("MIX_EXS") || "mix.exs")
 
     unless match?(%{file: ^mixfile}, Mix.ProjectStack.peek()) do
-      Code.load_file(System.get_env("MIX_EXS") || "mix.exs")
+      Code.require_file(System.get_env("MIX_EXS") || "mix.exs")
     end
 
     task = task || Mix.Project.config()[:default_task]
@@ -546,7 +546,7 @@ defmodule ElixirLS.Debugger.Server do
       %{name: name, file: file} = project
       :code.purge(name)
       :code.delete(name)
-      Code.load_file(file)
+      Code.require_file(file)
     end
   end
 

apps/elixir_ls_utils/test/support/mix_test.case.ex
@@ -126,7 +126,7 @@ defmodule ElixirLS.Utils.MixTest.Case do
     for %{name: module, file: file} <- stack do
       :code.purge(module)
       :code.delete(module)
-      Code.load_file(file)
+      Code.require_file(file)
     end
   end
 end

apps/language_server/lib/language_server/server.ex
@@ -110,11 +110,6 @@ defmodule ElixirLS.LanguageServer.Server do
     {:noreply, handle_build_result(status, diagnostics, state)}
   end
 
-  # Pre Elixir 1.6, we can't get diagnostics from builds
-  def handle_cast({:build_finished, _}, state) do
-    {:noreply, handle_build_result(:ok, [], state)}
-  end
-
   def handle_cast({:dialyzer_finished, diagnostics, build_ref}, state) do
     {:noreply, handle_dialyzer_result(diagnostics, build_ref, state)}
   end

apps/language_server/lib/language_server/build.ex
@@ -79,13 +79,7 @@ defmodule ElixirLS.LanguageServer.Build do
     msg =
       case error do
         {:shutdown, 1} ->
-          msg = "Build failed for unknown reason. See output log."
-
-          if Version.match?(System.version(), ">= 1.6.0-dev") do
-            msg
-          else
-            msg <> " Upgrade to Elixir 1.6 to see build errors and warnings."
-          end
+          "Build failed for unknown reason. See output log."
 
         _ ->
           Exception.format_exit(error)
@@ -124,23 +118,18 @@ defmodule ElixirLS.LanguageServer.Build do
       # Override build directory to avoid interfering with other dev tools
       Mix.ProjectStack.post_config(build_path: ".elixir_ls/build")
 
-      # If using Elixir 1.6 or higher, we can get diagnostics if Mixfile fails to load
+      # We can get diagnostics if Mixfile fails to load
       {status, diagnostics} =
-        if Version.match?(System.version(), ">= 1.6.0-dev") do
-          case Kernel.ParallelCompiler.compile([mixfile]) do
-            {:ok, _, warnings} ->
-              {:ok, Enum.map(warnings, &mixfile_diagnostic(&1, :warning))}
+        case Kernel.ParallelCompiler.compile([mixfile]) do
+          {:ok, _, warnings} ->
+            {:ok, Enum.map(warnings, &mixfile_diagnostic(&1, :warning))}
 
-            {:error, errors, warnings} ->
-              {
-                :error,
-                Enum.map(warnings, &mixfile_diagnostic(&1, :warning)) ++
-                  Enum.map(errors, &mixfile_diagnostic(&1, :error))
-              }
-          end
-        else
-          Code.load_file(mixfile)
-          {:ok, []}
+          {:error, errors, warnings} ->
+            {
+              :error,
+              Enum.map(warnings, &mixfile_diagnostic(&1, :warning)) ++
+                Enum.map(errors, &mixfile_diagnostic(&1, :error))
+            }
         end
 
       if status == :ok do

apps/language_server/lib/language_server/dialyzer/utils.ex
@@ -28,12 +28,9 @@ defmodule ElixirLS.LanguageServer.Dialyzer.Utils do
     expand_references(rest, exclude, result)
   end
 
-  # Mix.Utils.last_modified/1 returns a :calendar.universal_time() in Elixir < 1.7,
-  # otherwise posix time, so we normalize to a :calendar.universal_time()
+  # Mix.Utils.last_modified/1 returns a posix time, so we normalize to a :calendar.universal_time()
   def normalize_timestamp(timestamp) when is_integer(timestamp),
     do: :calendar.gregorian_seconds_to_datetime(timestamp + @epoch_gregorian_seconds)
-
-  def normalize_timestamp(timestamp), do: timestamp
 
   defp module_references(mod) do
     try do

apps/language_server/lib/language_server/providers/formatting.ex
@@ -9,7 +9,7 @@ defmodule ElixirLS.LanguageServer.Providers.Formatting do
   def format(source_file, uri, project_dir) do
     if can_format?(uri, project_dir) do
       file = SourceFile.path_from_uri(uri) |> Path.relative_to(project_dir)
-      opts = formatter_opts(file, project_dir)
+      opts = formatter_opts(file)
       formatted = IO.iodata_to_binary([Code.format_string!(source_file.text, opts), ?\n])
 
       response =
@@ -34,38 +34,12 @@ defmodule ElixirLS.LanguageServer.Providers.Formatting do
     file_path = file_uri |> SourceFile.path_from_uri() |> String.downcase()
     cwd = File.cwd!() |> String.downcase()
 
-    is_nil(project_dir) or not String.starts_with?(file_path, project_dir) or
+    not String.starts_with?(file_path, project_dir) or
       String.starts_with?(Path.absname(file_path), cwd)
   end
 
-  defp formatter_opts(for_file, project_dir) do
-    # Elixir 1.6.5+ has a function that returns formatter options, so we use that if available
-    if Code.ensure_loaded?(Mix.Tasks.Format) and
-         function_exported?(Mix.Tasks.Format, :formatter_opts_for_file, 1) do
-      Mix.Tasks.Format.formatter_opts_for_file(for_file)
-    else
-      read_formatter_exs(project_dir)
-    end
-  end
-
-  # TODO: Deprecate once Elixir 1.7 released
-  defp read_formatter_exs(project_dir) do
-    dot_formatter = Path.join(project_dir, ".formatter.exs")
-
-    if File.regular?(dot_formatter) do
-      {formatter_opts, _} = Code.eval_file(dot_formatter)
-
-      unless Keyword.keyword?(formatter_opts) do
-        Mix.raise(
-          "Expected #{inspect(dot_formatter)} to return a keyword list, " <>
-            "got: #{inspect(formatter_opts)}"
-        )
-      end
-
-      formatter_opts
-    else
-      []
-    end
+  defp formatter_opts(for_file) do
+    Mix.Tasks.Format.formatter_opts_for_file(for_file)
   end
 
   defp myers_diff_to_text_edits(myers_diff, starting_pos \\ {0, 0}) do

commit a05732949457d86bb3019c20f8f8b83d3e7f6f42

Fix the debugger
The debugger was inadvertantly broken in #93 which the change from the
deprecated ([2]) `Code.load_file/1` to `Code.require_file/1`. The compatability
docs for 1.10 [1] list both `Code.require_file/1` and `Code.compile_file/1` as
possible replacements for `Code.load_file/1`. In our case we need
`Code.compile_file/1` instead of `Code.require_file/1` since
`Code.require_file/1` tracks which files have been required and compiled and
does not compile them more than once. Specifically the docs state:

> If require_file/2 is called more than once with a given file, that file will
> be compiled only once

So since `Code.load_file/1` would always compile the file we need to use
`Code.compile_file/1` rather than `Code.require_file/1`. When
`Code.require_file/1` was used, the call in
`ElixirLS.Debugger.Server.change_env/1` would have no effect because the file
was previously required. By changing to `Code.compile_file/1` the project will
always be reloaded by compiling it's `mix.exs` file.

[1] https://hexdocs.pm/elixir/1.10.2/compatibility-and-deprecations.html#table-of-deprecations
[2] https://github.com/elixir-lang/elixir/pull/7201

apps/debugger/lib/debugger/server.ex
@@ -443,7 +443,7 @@ defmodule ElixirLS.Debugger.Server do
 
     # FIXME: Private API
     unless match?(%{file: ^mixfile}, Mix.ProjectStack.peek()) do
-      Code.require_file(System.get_env("MIX_EXS") || "mix.exs")
+      Code.compile_file(System.get_env("MIX_EXS") || "mix.exs")
     end
 
     task = task || Mix.Project.config()[:default_task]
@@ -555,7 +555,7 @@ defmodule ElixirLS.Debugger.Server do
       %{name: name, file: file} = project
       :code.purge(name)
       :code.delete(name)
-      Code.require_file(file)
+      Code.compile_file(file)
     end
   end
 

apps/elixir_ls_utils/test/support/mix_test.case.ex
@@ -129,7 +129,7 @@ defmodule ElixirLS.Utils.MixTest.Case do
     for %{name: module, file: file} <- stack do
       :code.purge(module)
       :code.delete(module)
-      Code.require_file(file)
+      Code.compile_file(file)
     end
   end
 