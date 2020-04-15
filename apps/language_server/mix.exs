1165471dfa0005cefc9abd727130c154eb7da141
Jason Axelson <jason.axelson@gmail.com>
2020-04-15 07:21:35 -1000

    Bump version to 0.3.3

--- apps/language_server/mix.exs
+++ apps/language_server/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.LanguageServer.Mixfile do
   def project do
     [
       app: :language_server,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: ">= 1.7.0",
       build_path: "../../_build",
       config_path: "config/config.exs",
