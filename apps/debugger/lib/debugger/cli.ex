03aa3a364f650cc703cb885e4d10fa517f140aa2
Jason Axelson <axelson@users.noreply.github.com>
2020-04-25 11:26:19 -1000

    Warn in debugger if on an unsupported version of elixir (#221)
    Fixes #158

--- apps/debugger/lib/debugger/cli.ex
+++ apps/debugger/lib/debugger/cli.ex
@@ -8,6 +8,22 @@ defmodule ElixirLS.Debugger.CLI do
     Application.ensure_all_started(:debugger, :permanent)
     IO.puts("Started ElixirLS debugger v#{Launch.debugger_version()}")
     Launch.print_versions()
+    warn_if_unsupported_version()
     WireProtocol.stream_packets(&Server.receive_packet/1)
+  end
+
+  # Debugging does not work on Elixir 1.10.0-1.10.2:
+  # https://github.com/elixir-lsp/elixir-ls/issues/158
+  defp warn_if_unsupported_version do
+    elixir_version = System.version()
+
+    if Version.match?(elixir_version, ">= 1.10.0") && Version.match?(elixir_version, "< 1.10.3") do
+      message =
+        "WARNING: Debugging is not supported on Elixir #{elixir_version}. Please upgrade" <>
+          " to at least 1.10.3\n" <>
+          "more info: https://github.com/elixir-lsp/elixir-ls/issues/158"
+
+      Output.print_err(message)
+    end
   end
 end
