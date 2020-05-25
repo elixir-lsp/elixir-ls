c530273ace5ca3616ae8fa99431a36d394c3712d
Jason Axelson <jason.axelson@gmail.com>
2020-05-24 16:37:44 -1000

    Remove keywords from completions
    They were being returned regardless of context which made them very noisy.
    
    Related:
    * https://github.com/elixir-lsp/elixir_sense/pull/99
    * https://github.com/elixir-lsp/elixir-ls/issues/251

--- apps/language_server/lib/language_server/providers/completion.ex
+++ apps/language_server/lib/language_server/providers/completion.ex
@@ -65,19 +65,6 @@ defmodule ElixirLS.LanguageServer.Providers.Completion do
                    "ExUnit.Assertions"
                  ])
 
-  @keywords %{
-    "end" => "end",
-    "do" => "do\n\t$0\nend",
-    "true" => "true",
-    "false" => "false",
-    "nil" => "nil",
-    "when" => "when",
-    "else" => "else\n\t$0",
-    "rescue" => "rescue\n\t$0",
-    "catch" => "catch\n\t$0",
-    "after" => "after\n\t$0"
-  }
-
   def trigger_characters do
     # VS Code's 24x7 autocompletion triggers automatically on alphanumeric characters. We add these
     # for "SomeModule." calls and @module_attrs
@@ -137,7 +124,6 @@ defmodule ElixirLS.LanguageServer.Providers.Completion do
       ElixirSense.suggestions(text, line + 1, character + 1)
       |> Enum.map(&from_completion_item(&1, context, options))
       |> Enum.concat(module_attr_snippets(context))
-      |> Enum.concat(keyword_completions(context))
 
     items_json =
       items
@@ -547,23 +533,6 @@ defmodule ElixirLS.LanguageServer.Providers.Completion do
   end
 
   defp module_attr_snippets(_), do: []
-
-  # These aren't really useful, to be honest, and it interferes with the auto-indentation
-  # for "else", but better to show them even if there's no good reason to use them
-  defp keyword_completions(%{prefix: prefix}) do
-    @keywords
-    |> Enum.filter(fn {keyword, _} -> String.starts_with?(keyword, prefix) end)
-    |> Enum.map(fn {keyword, snippet} ->
-      %__MODULE__{
-        label: keyword,
-        kind: :keyword,
-        detail: "keyword",
-        insert_text: snippet,
-        tags: [],
-        priority: 1
-      }
-    end)
-  end
 
   defp function_completion(info, context, options) do
     %{
