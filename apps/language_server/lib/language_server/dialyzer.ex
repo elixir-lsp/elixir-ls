db43cbc8cd319e4ee4d22c652ac48912039dc06f
Lukasz Samson <lukaszsamson@gmail.com>
2020-06-05 19:32:44 +0200

    warn if unable to remove temp file

--- apps/language_server/lib/language_server/dialyzer.ex
+++ apps/language_server/lib/language_server/dialyzer.ex
@@ -330,7 +330,13 @@ defmodule ElixirLS.LanguageServer.Dialyzer do
         |> Stream.run()
 
         for file <- removed_files do
-          File.rm(temp_file_path(root_path, file))
+          path = temp_file_path(root_path, file)
+          case File.rm(path) do
+            :ok -> :ok
+            {:error, :enoent} -> :ok
+            {:error, reason} ->
+              IO.warn("Unable to remove temporary file #{path}: #{inspect(reason)}")
+          end
         end
 
         temp_modules =
