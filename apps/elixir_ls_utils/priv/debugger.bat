4a20b102070186775a8b5e62a9e8b31407034673
Jason Axelson <jason.axelson@gmail.com>
2020-07-24 07:30:12 -1000

    Add +sbwt none to .bat files also

--- apps/elixir_ls_utils/priv/debugger.bat
+++ apps/elixir_ls_utils/priv/debugger.bat
@@ -1,4 +1,4 @@
 @echo off & setlocal enabledelayedexpansion
 
 SET ERL_LIBS=%~dp0;%ERL_LIBS%
-elixir -e "ElixirLS.Debugger.CLI.main()"
+elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none" -e "ElixirLS.Debugger.CLI.main()"
--- apps/elixir_ls_utils/priv/language_server.bat
+++ apps/elixir_ls_utils/priv/language_server.bat
@@ -1,4 +1,4 @@
 @echo off & setlocal enabledelayedexpansion
 
 SET ERL_LIBS=%~dp0;%ERL_LIBS%
-elixir -e "ElixirLS.LanguageServer.CLI.main()"
+elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none" -e "ElixirLS.LanguageServer.CLI.main()"
