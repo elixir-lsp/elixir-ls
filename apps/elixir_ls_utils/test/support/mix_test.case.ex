7e819ed2b615f2f387995e458758fa557d00e080
Lukasz Samson <lukaszsamson@gmail.com>
2020-08-14 14:59:34 +0200

    remove elixir 1.10-rc support

--- apps/elixir_ls_utils/test/support/mix_test.case.ex
+++ apps/elixir_ls_utils/test/support/mix_test.case.ex
@@ -139,7 +139,7 @@ defmodule ElixirLS.Utils.MixTest.Case do
   # FIXME: Private API
   defp clear_mix_cache do
     module =
-      if Version.match?(System.version(), ">= 1.10.0-rc.0") do
+      if Version.match?(System.version(), ">= 1.10.0") do
         Mix.State
       else
         Mix.ProjectStack
