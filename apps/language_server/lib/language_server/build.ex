da2a49686ddf477aa18a355adffdb67656e0059b
Thanabodee Charoenpiriyakij <thanabodee.c@linecorp.com>
2020-08-15 21:55:47 +0700

    Remove deps cached only when deps in mix file changed
    Remove deps cached every build can cause deps cached disappear in :ets.
    Fixes by move calling Mix.Project.clear_deps_cache/0 only when deps in
    mix.exs was change.
    
    Fixes #235

--- apps/language_server/lib/language_server/build.ex
+++ apps/language_server/lib/language_server/build.ex
@@ -13,15 +13,14 @@ defmodule ElixirLS.LanguageServer.Build do
               IO.puts("MIX_ENV: #{Mix.env()}")
               IO.puts("MIX_TARGET: #{Mix.target()}")
 
-              prev_deps = cached_deps()
-              :ok = Mix.Project.clear_deps_cache()
-
               case reload_project() do
                 {:ok, mixfile_diagnostics} ->
                   # FIXME: Private API
                   if Keyword.get(opts, :fetch_deps?) and
-                       Mix.Dep.load_on_environment([]) != prev_deps,
-                     do: fetch_deps()
+                       Mix.Dep.load_on_environment([]) != cached_deps() do
+                    :ok = Mix.Project.clear_deps_cache()
+                    fetch_deps()
+                  end
 
                   {status, diagnostics} = compile()
 
