bfa468117d853a30e36e2d3009b7b755e2fca6bf
E14 <1640391+E14@users.noreply.github.com>
2020-10-05 20:00:13 +0200

    Allow running a setup.bat script on Windows analogous to setup.sh on Linux
    Script is located at `%APPDATA%`/elixir_ls/setup.bat` which usually translates to `C:\Users\<username>\AppData\Roaming\elixir_ls`

--- apps/elixir_ls_utils/priv/debugger.bat
+++ apps/elixir_ls_utils/priv/debugger.bat
@@ -1,4 +1,8 @@
 @echo off & setlocal enabledelayedexpansion
 
+IF EXIST "%APPDATA%\elixir_ls\setup.bat" (
+    CALL "%APPDATA%\elixir_ls\setup.bat"
+)
+
 SET ERL_LIBS=%~dp0;%ERL_LIBS%
 elixir --erl "+sbwt none +sbwtdcpu none +sbwtdio none" -e "ElixirLS.Debugger.CLI.main()"
