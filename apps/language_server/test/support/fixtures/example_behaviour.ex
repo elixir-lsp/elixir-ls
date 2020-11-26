88c4f5c11a10d81460dcc904fe8e6cb7e96dfa63
Lukasz Samson <lukaszsamson@gmail.com>
2020-11-26 10:50:10 +0100

    add tests

--- apps/language_server/test/support/fixtures/example_behaviour.ex
+++ apps/language_server/test/support/fixtures/example_behaviour.ex
@@ -2,3 +2,13 @@ defmodule ElixirLS.LanguageServer.Fixtures.ExampleBehaviour do
   @callback greet_world() :: nil
   @callback build_greeting(name :: String.t()) :: String.t()
 end
+
+defmodule ElixirLS.LanguageServer.Fixtures.ExampleBehaviourImpl do
+  @behaviour ElixirLS.LanguageServer.Fixtures.ExampleBehaviour
+
+  @impl true
+  def greet_world(), do: nil
+
+  @impl true
+  def build_greeting(name), do: name
+end
