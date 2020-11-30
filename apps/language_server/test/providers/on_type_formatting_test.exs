449bf26fd73d488723ca91ea2d163797bc51edac
Jason Axelson <jason.axelson@gmail.com>
2020-11-30 08:15:32 -1000

    Add a couple more tests

--- apps/language_server/test/providers/on_type_formatting_test.exs
+++ apps/language_server/test/providers/on_type_formatting_test.exs
@@ -21,11 +21,40 @@ defmodule ElixirLS.LanguageServer.Providers.OnTypeFormattingTest do
             ]} = OnTypeFormatting.format(%SourceFile{text: text}, 1, 0, "\n", [])
   end
 
+  test "insert `end` after def `do`" do
+    text = """
+    defmodule TestA do
+      def myfun(a) do
+    end
+    """
+
+    assert {:ok,
+            [
+              %{
+                "newText" => "\n  end",
+                "range" => %{
+                  "start" => %{"character" => 0, "line" => 2},
+                  "end" => %{"character" => 0, "line" => 2}
+                }
+              }
+            ]} = OnTypeFormatting.format(%SourceFile{text: text}, 2, 0, "\n", [])
+  end
+
   test "don't  insert `end` after `do:`" do
     text = """
     for a <- b, do:
     """
 
     assert {:ok, nil} = OnTypeFormatting.format(%SourceFile{text: text}, 1, 0, "\n", [])
+  end
+
+  test "don't insert `end` after def `do: a`" do
+    text = """
+    defmodule TestA do
+      def myfun(a), do: a
+    end
+    """
+
+    assert {:ok, nil} = OnTypeFormatting.format(%SourceFile{text: text}, 2, 0, "\n", [])
   end
 end
