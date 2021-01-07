843e1a6cc964df82f604be94d0784773205758f3
Joshua Trees <jtrees@users.noreply.github.com>
2021-01-07 19:29:10 +0100

    Make wrapper script more robust (#445)
    This makes a setup possible in which a symlink points to the file
    (e.g. /usr/local/bin/elixir-ls -> /opt/elixir-ls/language_server.sh).
    
    Co-authored-by: Joshua Trees <me@jtrees.io>

--- apps/elixir_ls_utils/priv/language_server.sh
+++ apps/elixir_ls_utils/priv/language_server.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # Launches the language server. This script must be in the same directory as the compiled .ez archives.
 
-dir=$(dirname "$0")
+readlink_f () {
+  cd "$(dirname "$1")" > /dev/null
+  filename="$(basename "$1")"
+  if [ -h "$filename" ]; then
+    readlink_f "$(readlink "$filename")"
+  else
+    echo "$(pwd -P)/$filename"
+  fi
+}
+
+dir=$(dirname $(readlink_f "$0"))
 
 export ELS_MODE=language_server
 export ELS_SCRIPT="ElixirLS.LanguageServer.CLI.main()"
