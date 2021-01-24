572f071856b9902d222b5fd6f714c4a6715cba76
Étienne Lévesque <etienne.levesque16@gmail.com>
2021-01-24 16:28:43 -0500

    Fixes tests not compiling after first run

--- apps/language_server/test/fixtures/test_code_lens/mix.exs
+++ apps/language_server/test/fixtures/test_code_lens/mix.exs
@@ -2,7 +2,7 @@ defmodule TestCodeLens.MixProject do
   use Mix.Project
 
   def project do
-    [app: :references, version: "0.1.0"]
+    [app: :test_code_lens, version: "0.1.0"]
   end
 
   def application, do: []
