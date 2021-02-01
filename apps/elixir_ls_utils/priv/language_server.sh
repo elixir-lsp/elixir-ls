b6a154634f147f22a1c1a53f6b67fe83f7ed1566
Norbert Melzer <timmelzer@gmail.com>
2021-02-01 09:17:25 +0100

    add ELS_INSTALL_PREFIX to launch scripts

--- apps/elixir_ls_utils/priv/language_server.sh
+++ apps/elixir_ls_utils/priv/language_server.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Launches the language server. This script must be in the same directory as the compiled .ez archives.
 
-dir=$(dirname "$0")
+if [ -z "${ELS_INSTALL_PREFIX}" ]; then
+  dir=$(dirname "$0")
+else
+  dir=${ELS_INSTALL_PREFIX}
+fi
 
 export ELS_MODE=language_server
 export ELS_SCRIPT="ElixirLS.LanguageServer.CLI.main()"
