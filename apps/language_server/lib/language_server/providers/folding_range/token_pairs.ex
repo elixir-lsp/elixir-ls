a8c54a2cfb798f284c28fca7d475f63fc1832cf3
William Lanchantin <william.lanchantin@cargosense.com>
2021-02-18 14:36:40 -0500

    add `for` and `case`; add comments for clarity

--- apps/language_server/lib/language_server/providers/folding_range/token_pairs.ex
+++ apps/language_server/lib/language_server/providers/folding_range/token_pairs.ex
@@ -8,12 +8,16 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingRange.TokenPairs do
     "(": [:")"],
     "[": [:"]"],
     "{": [:"}"],
+    # do blocks
     do: [:catch, :rescue, :after, :else, :end],
     catch: [:rescue, :after, :else, :end],
     rescue: [:after, :else, :end],
     after: [:else, :end],
     else: [:end],
+    # other special forms
     with: [:do],
+    for: [:do],
+    case: [:do],
     fn: [:end]
   }
 
