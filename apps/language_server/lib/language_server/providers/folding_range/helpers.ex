888814a0fefd7b82e83b89008d267077d8096d6a
William Lanchantin <william.lanchantin@cargosense.com>
2021-02-19 12:14:06 -0500

    use pipes

--- apps/language_server/lib/language_server/providers/folding_range/helpers.ex
+++ apps/language_server/lib/language_server/providers/folding_range/helpers.ex
@@ -1,14 +1,14 @@
 defmodule ElixirLS.LanguageServer.Providers.FoldingRange.Helpers do
-  @moduledoc """
-  """
+  @moduledoc false
+
   def first_and_last_of_list([]), do: :empty_list
 
-  def first_and_last_of_list(list) when is_list(list) do
-    [head | tail] = list
-    do_falo_list(tail, head)
+  def first_and_last_of_list([head | tail]) do
+    tail
+    |> List.last()
+    |> case do
+      nil -> {head, head}
+      last -> {head, last}
+    end
   end
-
-  defp do_falo_list([], first), do: {first, first}
-  defp do_falo_list([last | []], first), do: {first, last}
-  defp do_falo_list([_ | tail], first), do: do_falo_list(tail, first)
 end
