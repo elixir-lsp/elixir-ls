4e8e1c0b541b4e4fcfe0ee9e68df32604e4add0d
Jason Axelson <jason.axelson@gmail.com>
2021-02-28 13:55:23 -1000

    Fix formatting

--- apps/elixir_ls_utils/test/changelog_test.exs
+++ apps/elixir_ls_utils/test/changelog_test.exs
@@ -3,12 +3,15 @@ defmodule ElixirLS.Utils.ChangelogTest do
 
   test "changelog pull requests are correctly linked" do
     contents = File.read!("../../CHANGELOG.md")
+
     String.split(contents, "\n", trim: true)
     |> Enum.each(fn line ->
       case Regex.run(~r/\/pull\/(\d+)/, line, capture: :all_but_first) do
         [pr_number] ->
           assert String.match?(line, ~r/\[.*#{pr_number}\]/)
-          _ -> nil
+
+        _ ->
+          nil
       end
     end)
   end
