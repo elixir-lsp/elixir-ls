e10d4b213747caa1912b907ca74cd90f65e372cb
Joshua Trees <jtrees@users.noreply.github.com>
2021-03-05 19:11:22 +0100

    Make wrapper script more robust... more robustly (#473)
    * Make wrapper script more robust... more robustly
    
    This makes a setup possible in which a symlink points to the file
    (e.g. /usr/local/bin/elixir-ls -> /opt/elixir-ls/language_server.sh).
    
    It's a rehash of a previous commit that was reverted since it broke
    setups where the language server was installed in a path containing
    spaces (e.g. /opt/Elixir Language Server/). This variant takes that
    possibility into account.
    
    * Avoid undefined behaviour in launch.sh
    
    I'm not really sure what could lead this to happen. Maybe some weird
    directory permissions with the read bit on and the exec bit off...
    
    Still good to be careful though.
    
    Co-authored-by: Łukasz Samson <lukaszsamson@gmail.com>
    
    Co-authored-by: Joshua Trees <me@jtrees.io>
    Co-authored-by: Łukasz Samson <lukaszsamson@gmail.com>

--- apps/elixir_ls_utils/priv/language_server.sh
+++ apps/elixir_ls_utils/priv/language_server.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # Launches the language server. This script must be in the same directory as the compiled .ez archives.
 
+readlink_f () {
+  cd "$(dirname "$1")" > /dev/null || exit 1
+  filename="$(basename "$1")"
+  if [ -h "$filename" ]; then
+    readlink_f "$(readlink "$filename")"
+  else
+    echo "$(pwd -P)/$filename"
+  fi
+}
+
 if [ -z "${ELS_INSTALL_PREFIX}" ]; then
-  dir=$(dirname "$0")
+  dir="$(dirname "$(readlink_f "$0")")"
 else
   dir=${ELS_INSTALL_PREFIX}
 fi
