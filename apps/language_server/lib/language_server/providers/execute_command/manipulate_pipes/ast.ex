eb969efae2a415e09404a371e8d3c401460ae367
Paulo Valente <polvalente@gmail.com>
2021-04-01 14:48:09 -0300

    feat: add custom commands for piping and unpiping text (#515)
    * feat: add basic to_pipe command core
    
    * refactor: rework text range finding algorithm
    
    * feat: make to_pipe/1 work correctly
    
    * refactor: move core ast code to separate module
    
    * feat: add from_pipe core implementation
    
    * feat: working implementation of from_pipe
    
    * refactor: remove regex scan
    
    * chore: comply with code review
    
    * test: make tests pass in older versions
    
    * fix: make test use correct elixir syntax
    
    * delete: remove stray file
    
    * chore: apply suggestions from code review
    
    Co-authored-by: Łukasz Samson <lukaszsamson@gmail.com>
    
    * refactor: use binary matching recursion
    
    * fix: add missing brackets
    
    * feat: add checks for avoiding false positives
    
    * chore: update apps/language_server/test/providers/execute_command/manipulate_pipes_test.exs
    
    Co-authored-by: Łukasz Samson <lukaszsamson@gmail.com>
    
    * feat: make code work with all lineseps
    
    * feat: do not pipe operators
    
    * refactor: do not replace newlines in ast module
    
    * fix: off-by-one errors and show code works with 2-byte characters
    
    * feat: deal with utf16 graphemes
    
    * test: make tests pass
    
    * fix: use escaped char as test name suffix
    
    * chore: format code
    
    * test: \r raises syntax error
    
    Co-authored-by: Łukasz Samson <lukaszsamson@gmail.com>

---
+++ apps/language_server/lib/language_server/providers/execute_command/manipulate_pipes/ast.ex
@@ -0,0 +1,67 @@
+defmodule ElixirLS.LanguageServer.Providers.ExecuteCommand.ManipulatePipes.AST do
+  @moduledoc """
+  AST manipulation helpers for the `ElixirLS.LanguageServer.Providers.ExecuteCommand.ManipulatePipes`\
+  command.
+  """
+
+  @doc "Parses a string and converts the first function call, pre-order depth-first, into a pipe."
+  def to_pipe(code_string) do
+    {piped_ast, _} =
+      code_string
+      |> Code.string_to_quoted!()
+      |> Macro.prewalk(%{has_piped: false}, &do_to_pipe/2)
+
+    Macro.to_string(piped_ast)
+  end
+
+  @doc "Parses a string and converts the first pipe call, post-order depth-first, into a function call."
+  def from_pipe(code_string) do
+    {unpiped_ast, _} =
+      code_string
+      |> Code.string_to_quoted!()
+      |> Macro.postwalk(%{has_unpiped: false}, fn
+        {:|>, line, [h, {{:., line, [{_, _, nil}]} = anonymous_function_node, line, t}]},
+        %{has_unpiped: false} = acc ->
+          {{anonymous_function_node, line, [h | t]}, Map.put(acc, :has_unpiped, true)}
+
+        {:|>, line, [left, {function, _, args}]}, %{has_unpiped: false} = acc ->
+          {{function, line, [left | args]}, Map.put(acc, :has_unpiped, true)}
+
+        node, acc ->
+          {node, acc}
+      end)
+
+    Macro.to_string(unpiped_ast)
+  end
+
+  defp do_to_pipe({:|>, line, [left, right]}, %{has_piped: false} = acc) do
+    {{:|>, line, [left |> do_to_pipe(acc) |> elem(0), right]}, Map.put(acc, :has_piped, true)}
+  end
+
+  defp do_to_pipe(
+         {{:., line, [{_, _, nil}]} = anonymous_function_node, _meta, [h | t]},
+         %{has_piped: false} = acc
+       ) do
+    {{:|>, line, [h, {anonymous_function_node, line, t}]}, Map.put(acc, :has_piped, true)}
+  end
+
+  defp do_to_pipe({{:., line, _args} = function, _meta, args}, %{has_piped: false} = acc)
+       when args != [] do
+    {{:|>, line, [hd(args), {function, line, tl(args)}]}, Map.put(acc, :has_piped, true)}
+  end
+
+  defp do_to_pipe({function, line, [h | t]} = node, %{has_piped: false} = acc)
+       when is_atom(function) and function not in [:., :__aliases__, :"::", :{}, :|>] and t != [] do
+    with :error <- Code.Identifier.binary_op(function),
+         :error <- Code.Identifier.unary_op(function) do
+      {{:|>, line, [h, {function, line, t}]}, Map.put(acc, :has_piped, true)}
+    else
+      _ ->
+        {node, acc}
+    end
+  end
+
+  defp do_to_pipe(node, acc) do
+    {node, acc}
+  end
+end
---
+++ apps/language_server/test/providers/execute_command/manipulate_pipes/ast_test.exs
@@ -0,0 +1,201 @@
+defmodule ElixirLS.LanguageServer.Providers.ExecuteCommand.ManipulatePipes.ASTTest do
+  use ExUnit.Case
+
+  alias ElixirLS.LanguageServer.Providers.ExecuteCommand.ManipulatePipes.AST
+
+  describe "to_pipe/1" do
+    test "single-line selection with two args in named function" do
+      assert AST.to_pipe("X.Y.Z.function_name(A.B.C.a(), b)") ==
+               "A.B.C.a() |> X.Y.Z.function_name(b)"
+    end
+
+    test "single-line selection with single arg in named function" do
+      assert AST.to_pipe("X.Y.Z.function_name(A.B.C.a())") == "A.B.C.a() |> X.Y.Z.function_name()"
+    end
+
+    test "single-line selection with two args in anonymous function" do
+      assert AST.to_pipe("function_name.(A.B.C.a(), b)") ==
+               "A.B.C.a() |> function_name.(b)"
+    end
+
+    test "single-line selection with single arg in anonymous function" do
+      assert AST.to_pipe("function_name.(A.B.C.a())") == "A.B.C.a() |> function_name.()"
+    end
+
+    test "multi-line selection with two args in named function" do
+      assert AST.to_pipe("""
+               X.Y.Z.function_name(
+               X.Y.Z.a(),
+               b,
+               c
+             )
+             """) == "X.Y.Z.a() |> X.Y.Z.function_name(b, c)"
+    end
+
+    test "multi-line with \r as separator and \r\n inside strings raises syntax error" do
+      # This test shows that the parser is limited to Elixir's parser's capabilities
+      assert_raise SyntaxError, fn ->
+        AST.to_pipe(~s{X.Y.Z.function_name(\r1,\r"asdf\nghjk")})
+      end
+    end
+
+    test "on 0 arity function call" do
+      assert AST.to_pipe("f.()") == "f.()"
+      assert AST.to_pipe("f()") == "f()"
+      assert AST.to_pipe("My.Nested.Module.f()") == "My.Nested.Module.f()"
+    end
+
+    test "on atom" do
+      assert AST.to_pipe(":asdf") == ":asdf"
+      assert AST.to_pipe("MyModule") == "MyModule"
+    end
+
+    test "calls with no parens" do
+      assert AST.to_pipe("f 1, 2") == "1 |> f(2)"
+      assert AST.to_pipe("MyModule.f 1, 2") == "1 |> MyModule.f(2)"
+    end
+
+    test "already a piped call (idempotency check)" do
+      assert AST.to_pipe("1 |> f(2)") == "1 |> f(2)"
+      assert AST.to_pipe("1 |> MyModule.f(2)") == "1 |> MyModule.f(2)"
+      assert AST.to_pipe("g(1) |> MyModule.f(2)") == "g(1) |> MyModule.f(2)"
+    end
+
+    test "does not pipe operators" do
+      assert AST.to_pipe("1 + 2") == "1 + 2"
+      assert AST.to_pipe("+2") == "+2"
+      # ensure that fully qualified operators are piped
+      assert AST.to_pipe("Kernel.+(1, 2)") == "1 |> Kernel.+(2)"
+    end
+  end
+
+  describe "from_pipe/1 single-line" do
+    test "does not change code without pipes" do
+      code = "f(a, b, c)"
+
+      assert AST.from_pipe(code) == code
+    end
+
+    test "three args in named function" do
+      piped = "a |> function_name(b, c) |> after_call"
+      unpiped = "function_name(a, b, c) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "pipe chain" do
+      piped = "a |> b() |> function_name(c, d) |> after_call"
+      unpiped = "b(a) |> function_name(c, d) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "pipe chain with no raw start" do
+      piped = "b(a) |> function_name(c, d) |> after_call"
+      unpiped = "function_name(b(a), c, d) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in named function" do
+      piped = "a |> function_name() |> after_call"
+      unpiped = "function_name(a) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "three args in anonymous function" do
+      piped = "a |> function_name.(b, c) |> after_call"
+      unpiped = "function_name.(a, b, c) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in anonymous function" do
+      piped = "a |> function_name.() |> after_call"
+      unpiped = "function_name.(a) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "three args in named function and statement before call" do
+      piped = "a |> function_name(b, c) |> after_call"
+      unpiped = "function_name(a, b, c) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in named function and statement before call" do
+      piped = "a |> function_name() |> after_call"
+      unpiped = "function_name(a) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "three args in anonymous function and statement before call" do
+      piped = "a |> function_name.(b, c) |> after_call"
+      unpiped = "function_name.(a, b, c) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in anonymous function and statement before call" do
+      piped = "a |> function_name.() |> after_call"
+      unpiped = "function_name.(a) |> after_call"
+      assert AST.from_pipe(piped) == unpiped
+    end
+  end
+
+  describe "from_pipe/1 multi-line" do
+    test "three args in named function" do
+      piped = """
+      a
+      |> function_name(b, c)
+      |> after_call
+      """
+
+      unpiped = "function_name(a, b, c) |> after_call"
+
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in named function" do
+      piped = """
+      a
+      |> function_name()
+      |> after_call
+      """
+
+      unpiped = "function_name(a) |> after_call"
+
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "three args in anonymous function" do
+      piped = """
+      a
+      |> function_name.(b, c)
+      |> after_call
+      """
+
+      unpiped = "function_name.(a, b, c) |> after_call"
+
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "single arg in anonymous function" do
+      piped = """
+      MyModule.f(a)
+      |> function_name.()
+      |> after_call
+      """
+
+      unpiped = "function_name.(MyModule.f(a)) |> after_call"
+
+      assert AST.from_pipe(piped) == unpiped
+    end
+
+    test "function call piped into another function" do
+      piped = """
+      f(a)
+      |> g(b, c)
+      |> after_call
+      """
+
+      unpiped = "g(f(a), b, c) |> after_call"
+
+      assert AST.from_pipe(piped) == unpiped
+    end
+  end
+end
