fdf5265e40118ab226b6b16d1dc6a6d3723aa0b1
Jason Axelson <jason.axelson@gmail.com>
2021-04-06 07:30:43 -1000

    Release version 0.7.0

--- apps/elixir_ls_debugger/mix.exs
+++ apps/elixir_ls_debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :elixir_ls_debugger,
-      version: "0.6.5",
+      version: "0.7.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
