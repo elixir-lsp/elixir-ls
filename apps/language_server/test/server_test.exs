47f11bd4570585c7108a58625104ed1582cfeaaa
Lukasz Samson <lukaszsamson@gmail.com>
2021-04-14 22:00:08 +0200

    fix test on windows

--- apps/language_server/test/server_test.exs
+++ apps/language_server/test/server_test.exs
@@ -1191,7 +1191,11 @@ defmodule ElixirLS.LanguageServer.ServerTest do
       file_uri = SourceFile.path_to_uri(file_path)
       file_absolute_path = SourceFile.path_from_uri(file_uri)
       text = File.read!(file_path)
-      project_dir = SourceFile.path_from_uri(root_uri())
+
+      project_dir =
+        root_uri()
+        |> SourceFile.path_from_uri()
+        |> Path.absname()
 
       initialize(server)
 
