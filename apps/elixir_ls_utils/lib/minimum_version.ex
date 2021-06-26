7722024a1c3534f8d830916d5c664e5928c71796
Jason Axelson <jason.axelson@gmail.com>
2021-06-26 10:17:22 -1000

    Fix formatting

--- apps/elixir_ls_utils/lib/minimum_version.ex
+++ apps/elixir_ls_utils/lib/minimum_version.ex
@@ -3,7 +3,8 @@ defmodule ElixirLS.Utils.MinimumVersion do
     otp_release = String.to_integer(System.otp_release())
 
     if otp_release < 22 do
-      {:error, "Erlang OTP releases below 22 are not supported (Currently running OTP #{otp_release})"}
+      {:error,
+       "Erlang OTP releases below 22 are not supported (Currently running OTP #{otp_release})"}
     else
       :ok
     end
@@ -13,7 +14,8 @@ defmodule ElixirLS.Utils.MinimumVersion do
     if Version.match?(System.version(), ">= 1.10.0") do
       :ok
     else
-      {:error, "Elixir versions below 1.10 are not supported. (Currently running v#{System.version()})"}
+      {:error,
+       "Elixir versions below 1.10 are not supported. (Currently running v#{System.version()})"}
     end
   end
 end
