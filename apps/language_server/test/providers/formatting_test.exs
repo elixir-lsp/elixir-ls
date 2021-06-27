c84e455f23710ffa1bd9452ac49809502d000c2e
Lukasz Samson <lukaszsamson@gmail.com>
2021-06-27 15:54:49 +0200

    add tests

--- apps/language_server/test/providers/formatting_test.exs
+++ apps/language_server/test/providers/formatting_test.exs
@@ -6,6 +6,106 @@ defmodule ElixirLS.LanguageServer.Providers.FormattingTest do
   alias ElixirLS.LanguageServer.Test.FixtureHelpers
 
   @tag :fixture
+  test "no mixfile" do
+    in_fixture(Path.join(__DIR__, ".."), "no_mixfile", fn ->
+      path = "lib/file.ex"
+      uri = SourceFile.path_to_uri(path)
+
+      text = """
+      defmodule MyModule do
+        require Logger
+
+        def dummy_function() do
+          Logger.info "dummy"
+        end
+      end
+      """
+
+      source_file = %SourceFile{
+        text: text,
+        version: 1,
+        dirty?: true
+      }
+
+      project_dir = maybe_convert_path_separators(FixtureHelpers.get_path("no_mixfile"))
+
+      assert {:ok, changes} = Formatting.format(source_file, uri, project_dir)
+
+      assert changes == [
+               %{
+                 "newText" => ")",
+                 "range" => %{
+                   "end" => %{"character" => 23, "line" => 4},
+                   "start" => %{"character" => 23, "line" => 4}
+                 }
+               },
+               %{
+                 "newText" => "(",
+                 "range" => %{
+                   "end" => %{"character" => 16, "line" => 4},
+                   "start" => %{"character" => 15, "line" => 4}
+                 }
+               }
+             ]
+
+      assert Enum.all?(changes, fn change ->
+               assert_position_type(change["range"]["end"]) and
+                 assert_position_type(change["range"]["start"])
+             end)
+    end)
+  end
+
+  @tag :fixture
+  test "no project dir" do
+    in_fixture(Path.join(__DIR__, ".."), "no_mixfile", fn ->
+      path = "lib/file.ex"
+      uri = SourceFile.path_to_uri(path)
+
+      text = """
+      defmodule MyModule do
+        require Logger
+
+        def dummy_function() do
+          Logger.info "dummy"
+        end
+      end
+      """
+
+      source_file = %SourceFile{
+        text: text,
+        version: 1,
+        dirty?: true
+      }
+
+      project_dir = nil
+
+      assert {:ok, changes} = Formatting.format(source_file, uri, project_dir)
+
+      assert changes == [
+               %{
+                 "newText" => ")",
+                 "range" => %{
+                   "end" => %{"character" => 23, "line" => 4},
+                   "start" => %{"character" => 23, "line" => 4}
+                 }
+               },
+               %{
+                 "newText" => "(",
+                 "range" => %{
+                   "end" => %{"character" => 16, "line" => 4},
+                   "start" => %{"character" => 15, "line" => 4}
+                 }
+               }
+             ]
+
+      assert Enum.all?(changes, fn change ->
+               assert_position_type(change["range"]["end"]) and
+                 assert_position_type(change["range"]["start"])
+             end)
+    end)
+  end
+
+  @tag :fixture
   test "Formats a file with LF line endings" do
     in_fixture(Path.join(__DIR__, ".."), "formatter", fn ->
       path = "lib/file.ex"
