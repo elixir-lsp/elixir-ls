d55925e199f9914b208a07dad6dfa622ebec3159
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-08-12 09:44:11 -0300

    Watch heex and sface templates

--- apps/language_server/lib/language_server/server.ex
+++ apps/language_server/lib/language_server/server.ex
@@ -71,7 +71,7 @@ defmodule ElixirLS.LanguageServer.Server do
     end
   end
 
-  @watched_extensions [".ex", ".exs", ".erl", ".hrl", ".yrl", ".xrl", ".eex", ".leex"]
+  @watched_extensions [".ex", ".exs", ".erl", ".hrl", ".yrl", ".xrl", ".eex", ".leex", ".heex", ".sface"]
 
   ## Client API
 
