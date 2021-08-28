5f07a740ffd1302dfd42aeb3075b02249057418f
sabiwara <sabiwara@gmail.com>
2021-08-28 21:57:20 +0200

    Use line from missing terminator hint (#588)
    * Use line from missing terminator hint
    
    * Fix formatter error
    
    * Remove mix files
    
    Co-authored-by: Jason Axelson <jason.axelson@gmail.com>

--- apps/language_server/.formatter.exs
+++ apps/language_server/.formatter.exs
@@ -1,6 +1,12 @@
+impossible_to_format = ["test/fixtures/token_missing_error/lib/has_error.ex"]
+
 [
-  inputs: [
-    "*.exs",
-    "{lib,test,config}/**/*.{ex,exs}"
-  ]
+  inputs:
+    Enum.flat_map(
+      [
+        "*.exs",
+        "{lib,test,config}/**/*.{ex,exs}"
+      ],
+      &Path.wildcard(&1, match_dot: true)
+    ) -- impossible_to_format
 ]
