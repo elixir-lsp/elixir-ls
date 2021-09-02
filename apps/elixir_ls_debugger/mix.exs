db38656abf1b69d2b36860c031596cf252625e41
Jason Axelson <axelson@users.noreply.github.com>
2021-09-01 20:58:08 -0700

    Bump version to 0.8.1 (#602)

--- apps/elixir_ls_debugger/mix.exs
+++ apps/elixir_ls_debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :elixir_ls_debugger,
-      version: "0.8.0",
+      version: "0.8.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
