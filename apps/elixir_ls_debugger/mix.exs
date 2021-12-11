977fae77bfecd695bae30da6643d43c12a885d32
Thanabodee Charoenpiriyakij <thanabodee.c@linecorp.com>
2021-12-11 21:22:18 +0700

    Bump all applications to 0.9.0
    Fixes #637

--- apps/elixir_ls_debugger/mix.exs
+++ apps/elixir_ls_debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :elixir_ls_debugger,
-      version: "0.8.2",
+      version: "0.9.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
