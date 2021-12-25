72115898a50632d7e8118e7bf32499a0c41c4f7a
Lukasz Samson <lukaszsamson@gmail.com>
2021-12-25 11:39:24 +0100

    add restart custom command

--- apps/language_server/lib/language_server/providers/execute_command.ex
+++ apps/language_server/lib/language_server/providers/execute_command.ex
@@ -3,29 +3,31 @@ defmodule ElixirLS.LanguageServer.Providers.ExecuteCommand do
   Adds a @spec annotation to the document when the user clicks on a code lens.
   """
 
+  alias ElixirLS.LanguageServer.Providers.ExecuteCommand, as: C
+
+  @handlers %{
+    "spec" => C.ApplySpec,
+    "expandMacro" => C.ExpandMacro,
+    "manipulatePipes" => C.ManipulatePipes,
+    "restart" => C.Restart
+  }
+
   @callback execute([any], %ElixirLS.LanguageServer.Server{}) ::
               {:ok, any} | {:error, atom, String.t()}
 
-  def execute(command, args, state) do
-    handler =
-      case command do
-        "spec:" <> _ ->
-          ElixirLS.LanguageServer.Providers.ExecuteCommand.ApplySpec
-
-        "expandMacro:" <> _ ->
-          ElixirLS.LanguageServer.Providers.ExecuteCommand.ExpandMacro
-
-        "manipulatePipes:" <> _ ->
-          ElixirLS.LanguageServer.Providers.ExecuteCommand.ManipulatePipes
-
-        _ ->
-          nil
-      end
-
-    if handler do
+  def execute(command_with_server_id, args, state) do
+    with [command, _server_id] <- String.split(command_with_server_id, ":"),
+         handler when not is_nil(handler) <- Map.get(@handlers, command) do
       handler.execute(args, state)
     else
-      {:error, :invalid_request, nil}
+      _ ->
+        {:error, :invalid_request, nil}
+    end
+  end
+
+  def get_commands(server_instance_id) do
+    for {k, _v} <- @handlers do
+      "#{k}:#{server_instance_id}"
     end
   end
 end
