f28c9e4bbf259360e4e9b40e480ec946e4477249
Lukasz Samson <lukaszsamson@gmail.com>
2022-01-09 16:35:38 +0100

    extract and test erlang binding processing
    fix some isses when var is usend more than 10 times
    add explicite ordering by variable instance
    discard underscored variables

---
+++ apps/elixir_ls_debugger/lib/debugger/binding.ex
@@ -0,0 +1,38 @@
+defmodule ElixirLS.Debugger.Binding do
+  def to_elixir_variable_names(bindings) do
+    bindings
+    |> Enum.group_by(fn {key, _} -> get_elixir_variable(key) end)
+    # filter out underscore binding as those are invalid in elixir
+    |> Enum.reject(fn {classic_key, _} ->
+      classic_key |> Atom.to_string() |> String.starts_with?("_")
+    end)
+    |> Enum.map(fn {classic_key, list} ->
+      # assume binding with highest number is the current one
+      # this may not be allways true, e.g. in
+      # a = 5
+      # if true do
+      #   a = 4
+      # end
+      # results in _a@1 = 5 and _a@2 = 4
+      # but we have no way of telling which one is current
+      {_, last_value} = list |> Enum.max_by(fn {key, _} -> key end)
+      {classic_key, last_value}
+    end)
+  end
+
+  def get_elixir_variable(key) do
+    # binding is present with prefix _ and postfix @
+    # for example _key@1 and _value@1 are representations of current function variables
+    key
+    |> Atom.to_string()
+    |> String.replace(~r/_(.*)@\d+/, "\\1")
+    |> String.to_atom()
+  end
+
+  def get_number(key) do
+    key
+    |> Atom.to_string()
+    |> String.replace(~r/_.*@(\d+)/, "\\1")
+    |> String.to_integer()
+  end
+end
