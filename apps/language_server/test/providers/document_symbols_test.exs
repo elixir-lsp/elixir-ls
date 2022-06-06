e3278b82d4efc8bfe19d6ebe70cfa6b5d0c6596d
Lukasz Samson <lukaszsamson@gmail.com>
2022-06-06 06:35:26 +0200

    wip

--- apps/language_server/test/providers/document_symbols_test.exs
+++ apps/language_server/test/providers/document_symbols_test.exs
@@ -3032,7 +3032,7 @@ defmodule ElixirLS.LanguageServer.Providers.DocumentSymbolsTest do
                name: "MISSING_PROTOCOL_NAME",
                range: %{
                  "start" => %{"line" => 0, "character" => 0},
-                 "end" => %{"line" => 0, "character" => 0}
+                 "end" => %{"line" => 1, "character" => 3}
                },
                selectionRange: %{
                  "start" => %{"line" => 0, "character" => 0},
