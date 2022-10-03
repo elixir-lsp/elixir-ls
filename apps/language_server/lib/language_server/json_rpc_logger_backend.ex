7406463596edf55204ac242bf6a90f1f90f611da
Lukasz Samson <lukaszsamson@gmail.com>
2022-10-03 23:15:39 +0200

    remove device

--- apps/language_server/lib/language_server/json_rpc_logger_backend.ex
+++ apps/language_server/lib/language_server/json_rpc_logger_backend.ex
@@ -20,30 +20,20 @@ defmodule Logger.Backends.JsonRpc do
       Setting `:metadata` to `:all` prints all metadata. See
       the "Metadata" section for more information.
 
-    * `:device` - the device to log error messages to. Defaults to
-      `:user` but can be changed to something else such as `:standard_error`.
-
   """
 
   @behaviour :gen_event
 
-  defstruct device: nil,
-            format: nil,
+  defstruct format: nil,
             level: nil,
             metadata: nil,
-            output: nil,
-            ref: nil
+            output: nil
 
   @impl true
   def init(:json_rpc) do
     config = Application.get_env(:logger, :json_rpc)
-    device = Keyword.get(config, :device, :user)
 
-    if Process.whereis(device) do
-      {:ok, init(config, %__MODULE__{})}
-    else
-      {:error, :ignore}
-    end
+    {:ok, init(config, %__MODULE__{})}
   end
 
   def init({__MODULE__, opts}) when is_list(opts) do
@@ -58,7 +48,7 @@ defmodule Logger.Backends.JsonRpc do
 
   @impl true
   def handle_event({level, _gl, {Logger, msg, ts, md}}, state) do
-    %{level: log_level, ref: ref} = state
+    %{level: log_level} = state
 
     {:erl_level, level} = List.keyfind(md, :erl_level, 0, {:erl_level, level})
 
@@ -66,13 +56,13 @@ defmodule Logger.Backends.JsonRpc do
       not meet_level?(level, log_level) ->
         {:ok, state}
 
-      is_nil(ref) ->
+      true ->
         {:ok, log_event(level, msg, ts, md, state)}
     end
   end
 
   def handle_event(:flush, state) do
-    {:ok, flush(state)}
+    {:ok, state}
   end
 
   def handle_event(_, state) do
@@ -80,14 +70,6 @@ defmodule Logger.Backends.JsonRpc do
   end
 
   @impl true
-  def handle_info({:io_reply, ref, msg}, %{ref: ref} = state) do
-    {:ok, handle_io_reply(msg, state)}
-  end
-
-  def handle_info({:DOWN, ref, _, pid, reason}, %{ref: ref}) do
-    raise "device #{inspect(pid)} exited: " <> Exception.format_exit(reason)
-  end
-
   def handle_info(_, state) do
     {:ok, state}
   end
@@ -118,7 +100,6 @@ defmodule Logger.Backends.JsonRpc do
 
   defp init(config, state) do
     level = Keyword.get(config, :level)
-    device = Keyword.get(config, :device, :user)
     format = Logger.Formatter.compile(Keyword.get(config, :format))
     metadata = Keyword.get(config, :metadata, []) |> configure_metadata()
 
@@ -126,8 +107,7 @@ defmodule Logger.Backends.JsonRpc do
       state
       | format: format,
         metadata: metadata,
-        level: level,
-        device: device
+        level: level
     }
   end
 
@@ -140,41 +120,9 @@ defmodule Logger.Backends.JsonRpc do
     end)
   end
 
-  defp log_event(level, msg, ts, md, %{device: device} = state) do
+  defp log_event(level, msg, ts, md, state) do
     output = format_event(level, msg, ts, md, state)
-    %{state | ref: async_io(device, output), output: output}
-  end
-
-  defp async_io(name, output) when is_atom(name) do
-    case Process.whereis(name) do
-      device when is_pid(device) ->
-        async_io(device, output)
-
-      nil ->
-        raise "no device registered with the name #{inspect(name)}"
-    end
-  end
-
-  defp async_io(device, output) when is_pid(device) do
-    ref = Process.monitor(device)
-    send(device, {:io_request, self(), ref, {:put_chars, :unicode, output}})
-    ref
-  end
-
-  defp await_io(%{ref: nil} = state), do: state
-
-  defp await_io(%{ref: ref} = state) do
-    receive do
-      {:io_reply, ^ref, :ok} ->
-        handle_io_reply(:ok, state)
-
-      {:io_reply, ^ref, error} ->
-        handle_io_reply(error, state)
-        |> await_io()
-
-      {:DOWN, ^ref, _, pid, reason} ->
-        raise "device #{inspect(pid)} exited: " <> Exception.format_exit(reason)
-    end
+    %{state | output: output}
   end
 
   defp format_event(level, msg, ts, md, state) do
@@ -195,33 +143,5 @@ defmodule Logger.Backends.JsonRpc do
         :error -> acc
       end
     end)
-  end
-
-  defp retry_log(error, %{device: device, ref: ref, output: dirty} = state) do
-    Process.demonitor(ref, [:flush])
-
-    try do
-      :unicode.characters_to_binary(dirty)
-    rescue
-      ArgumentError ->
-        clean = ["failure while trying to log malformed data: ", inspect(dirty), ?\n]
-        %{state | ref: async_io(device, clean), output: clean}
-    else
-      {_, good, bad} ->
-        clean = [good | Logger.Formatter.prune(bad)]
-        %{state | ref: async_io(device, clean), output: clean}
-
-      _ ->
-        # A well behaved IO device should not error on good data
-        raise "failure while logging json_rpc messages: " <> inspect(error)
-    end
-  end
-
-  defp flush(%{ref: nil} = state), do: state
-
-  defp flush(state) do
-    state
-    |> await_io()
-    |> flush()
   end
 end
