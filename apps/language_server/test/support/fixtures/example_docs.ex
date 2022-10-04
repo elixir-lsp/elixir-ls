b9414214c03cfad2d54b2180af8aa6a2b47d0758
George Rodrigues <georgerodrigues@live.com.au>
2022-10-04 11:28:14 -0300

    docs: fix typos (#737)

--- apps/language_server/test/support/fixtures/example_docs.ex
+++ apps/language_server/test/support/fixtures/example_docs.ex
@@ -2,7 +2,7 @@ defmodule ElixirLS.LanguageServer.Fixtures.ExampleDocs do
   @doc """
   The summary
 
-  Ths rest
+  This rest
   """
   @spec add(a_big_name :: integer, b_big_name :: integer) :: integer
   def add(a, b) do
