a7930bb570472930a9d04efe64f2e9921fe75da0
Lukasz Samson <lukaszsamson@gmail.com>
2022-10-12 22:15:26 +0200

    use DAP compliant output category in debugger
    stdout and stderr is reserved for debuggee, console and important for debugger

--- apps/elixir_ls_debugger/lib/debugger/cli.ex
+++ apps/elixir_ls_debugger/lib/debugger/cli.ex
@@ -3,18 +3,18 @@ defmodule ElixirLS.Debugger.CLI do
   alias ElixirLS.Debugger.{Output, Server}
 
   def main do
-    WireProtocol.intercept_output(&Output.print/1, &Output.print_err/1)
+    WireProtocol.intercept_output(&Output.debuggee_out/1, &Output.debuggee_err/1)
     Launch.start_mix()
     {:ok, _} = Application.ensure_all_started(:elixir_ls_debugger, :permanent)
 
-    IO.puts("Started ElixirLS Debugger v#{Launch.debugger_version()}")
+    Output.debugger_console("Started ElixirLS Debugger v#{Launch.debugger_version()}")
     versions = Launch.get_versions()
 
-    IO.puts(
+    Output.debugger_console(
       "ElixirLS Debugger built with elixir #{versions.compile_elixir_version} on OTP #{versions.compile_otp_version}"
     )
 
-    IO.puts(
+    Output.debugger_console(
       "Running on elixir #{versions.current_elixir_version} on OTP #{versions.current_otp_version}"
     )
 
@@ -25,11 +25,11 @@ defmodule ElixirLS.Debugger.CLI do
 
   defp warn_if_unsupported_version do
     with {:error, message} <- ElixirLS.Utils.MinimumVersion.check_elixir_version() do
-      Output.print_err("WARNING: " <> message)
+      Output.debugger_important("WARNING: " <> message)
     end
 
     with {:error, message} <- ElixirLS.Utils.MinimumVersion.check_otp_version() do
-      Output.print_err("WARNING: " <> message)
+      Output.debugger_important("WARNING: " <> message)
     end
   end
 end
