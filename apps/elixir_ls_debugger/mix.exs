2772a5af81231637dbe6321f88277e01a1880a3b
Lukasz Samson <lukaszsamson@gmail.com>
2023-01-08 15:32:28 +0100

    bump version

--- apps/elixir_ls_debugger/mix.exs
+++ apps/elixir_ls_debugger/mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirLS.Debugger.Mixfile do
   def project do
     [
       app: :elixir_ls_debugger,
-      version: "0.12.0",
+      version: "0.13.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -25,18 +25,6 @@ defmodule ElixirLS.Mixfile do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
   end
