0efd9585ad89726abd4a95adcfb2f5d8f39940b3
Steve Cohen <scohen@scohen.org>
2023-03-16 02:22:30 -0700

    Made .formatter.exs inputs relocatable (#846)
    * Made .formatter.exs inputs relocatable
    
    The .formatter.exs defined paths via a wildcard, but this would only
    work if it was executed from the current directory. If it was run from
    the topmost directory, none of the paths would be found, and the
    inputs would only contain the `.exs` files in the current directory.
    
    I also removed config from the glob, as this is in an umbrella
    project, and umbrella apps have a centralized config which isn't in
    this directory.
    
    * Only append the formatter directory if the path is relative
    
    * Need to make the ignores absolute paths as well
    
    * Removed forwared slashes

--- apps/language_server/.formatter.exs
+++ apps/language_server/.formatter.exs
@@ -1,6 +1,15 @@
+current_directory = Path.dirname(__ENV__.file)
+
 impossible_to_format = [
-  "test/fixtures/token_missing_error/lib/has_error.ex",
-  "test/fixtures/project_with_tests/test/error_test.exs"
+  Path.join([current_directory, "test", "fixtures", "token_missing_error", "lib", "has_error.ex"]),
+  Path.join([
+    current_directory,
+    "test",
+    "fixtures",
+    "project_with_tests",
+    "test",
+    "error_test.exs"
+  ])
 ]
 
 deps =
@@ -29,8 +38,8 @@ proto_dsl = [
   inputs:
     Enum.flat_map(
       [
-        "*.exs",
-        "{lib,test,config}/**/*.{ex,exs}"
+        Path.join(current_directory, "*.exs"),
+        Path.join(current_directory, "{lib,test}/**/*.{ex,exs}")
       ],
       &Path.wildcard(&1, match_dot: true)
     ) -- impossible_to_format
