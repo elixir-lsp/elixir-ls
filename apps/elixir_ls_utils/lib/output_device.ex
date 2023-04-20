elixir-lsp/elixir-ls

commit 4ae6e22a9c01671192f827b1d0dcf5c573852061

Improve debugger stability (#457)
* remove legacy io_request handlers

we don't support OTP < R15B

* rescue MatchError in :int calls

Fixes #455

* make output device better conform to erlang I/O protocol

see https://erlang.org/doc/apps/stdlib/io_protocol.html for details

* return WireProtocol.send error to the caller

no need to IO.warn if write fails

* we are redirection stderr to stdout, use stdout as underlying device

* inspect error

* monitor debugged processes

add test for mix task exit
Fixes #454

* avoid debugger crashes when handling requests for no longer existing thread, frame and variable ids

Fixes #452

* add test

* forbid changes of underlying device opts

* refactor and add tests coverage to invalid requests

* Map.pop! is available since elixir 1.10

* run formatter

apps/elixir_ls_utils/lib/output_device.ex
@@ -1,7 +1,7 @@
 defmodule ElixirLS.Utils.OutputDevice do
   @moduledoc """
   Intercepts IO request messages and forwards them to the Output server to be sent as events to
-  the IDE.
+  the IDE. Implements Erlang I/O Protocol https://erlang.org/doc/apps/stdlib/io_protocol.html
 
   In order to send console output to Visual Studio Code, the debug adapter needs to send events
   using the usual wire protocol. In order to intercept the debugged code's output, we replace the
@@ -10,65 +10,121 @@ defmodule ElixirLS.Utils.OutputDevice do
   server with the correct category ("stdout" or "stderr").
   """
 
-  use GenServer
+  @opts binary: true, encoding: :unicode
 
   ## Client API
 
-  def start_link(device, output_fn, opts \\ []) do
-    GenServer.start_link(__MODULE__, {device, output_fn}, opts)
+  def start_link(device, output_fn) do
+    Task.start_link(fn -> loop({device, output_fn}) end)
   end
 
-  ## Server callbacks
+  def get_opts, do: @opts
 
-  @impl GenServer
-  def init({device, output_fn}) do
-    {:ok, {device, output_fn}}
-  end
+  ## Implementation
 
-  @impl GenServer
-  def handle_info({:io_request, from, reply_as, {:put_chars, _encoding, characters}}, s) do
-    output(from, reply_as, characters, s)
-    {:noreply, s}
-  end
+  defp loop(state) do
+    receive do
+      {:io_request, from, reply_as, request} ->
+        result = io_request(request, state, reply_as)
+        send(from, {:io_reply, reply_as, result})
 
-  @impl GenServer
-  def handle_info({:io_request, from, reply_as, {:put_chars, characters}}, s) do
-    output(from, reply_as, characters, s)
-    {:noreply, s}
-  end
-
-  @impl GenServer
-  def handle_info({:io_request, from, reply_as, {:put_chars, _encoding, module, func, args}}, s) do
-    output(from, reply_as, apply(module, func, args), s)
-    {:noreply, s}
-  end
-
-  @impl GenServer
-  def handle_info({:io_request, from, reply_as, {:put_chars, module, func, args}}, s) do
-    output(from, reply_as, apply(module, func, args), s)
-    {:noreply, s}
-  end
-
-  @impl GenServer
-  def handle_info({:io_request, from, reply_as, {:requests, reqs}}, s) do
-    for req <- reqs do
-      handle_info({:io_request, from, reply_as, req}, s)
+        loop(state)
     end
-
-    {:noreply, s}
   end
 
-  # Any other message (get_geometry, set_opts, etc.) goes directly to original device
-  @impl GenServer
-  def handle_info(msg, {device, _} = s) do
-    send(device, msg)
-    {:noreply, s}
+  defp send_to_output(encoding, characters, {_device, output_fn}) do
+    # convert to unicode binary if necessary
+    case wrap_characters_to_binary(characters, encoding) do
+      binary when is_binary(binary) ->
+        output_fn.(binary)
+
+      _ ->
+        {:error, :put_chars}
+    end
   end
 
-  ## Helpers
-
-  defp output(from, reply_as, characters, {_, output_fn}) do
-    output_fn.(IO.iodata_to_binary(characters))
-    send(from, {:io_reply, reply_as, :ok})
+  defp io_request({:put_chars, encoding, characters}, state, _reply_as) do
+    send_to_output(encoding, characters, state)
   end
+
+  defp io_request({:put_chars, encoding, module, func, args}, state, _reply_as) do
+    # apply mfa to get binary or list
+    # return error in other cases
+    try do
+      case apply(module, func, args) do
+        characters when is_list(characters) or is_binary(characters) ->
+          send_to_output(encoding, characters, state)
+
+        _ ->
+          {:error, :put_chars}
+      end
+    catch
+      _, _ -> {:error, :put_chars}
+    end
+  end
+
+  defp io_request({:requests, list}, state, reply_as) do
+    # process request sequentially until error or end of data
+    # return last result
+    case io_requests(list, {:ok, :ok}, state, reply_as) do
+      :ok -> :ok
+      {:error, error} -> {:error, error}
+      other -> {:ok, other}
+    end
+  end
+
+  defp io_request(:getopts, _state, _reply_as) do
+    @opts
+  end
+
+  defp io_request({:setopts, new_opts}, _state, _reply_as) do
+    validate_otps(new_opts, {:ok, 0})
+  end
+
+  defp io_request(unknown, {device, _output_fn}, reply_as) do
+    # forward requests to underlying device
+    send(device, {:io_request, self(), reply_as, unknown})
+
+    receive do
+      {:io_reply, ^reply_as, reply} -> reply
+    end
+  end
+
+  defp io_requests(_, {:error, error}, _, _), do: {:error, error}
+
+  defp io_requests([request | rest], _, state, reply_as) do
+    result = io_request(request, state, reply_as)
+    io_requests(rest, result, state, reply_as)
+  end
+
+  defp io_requests([], result, _, _), do: result
+
+  defp wrap_characters_to_binary(bin, :unicode) when is_binary(bin), do: bin
+
+  defp wrap_characters_to_binary(chars, from) do
+    # :unicode.characters_to_binary may throw, return error or incomplete result
+    try do
+      case :unicode.characters_to_binary(chars, from, :unicode) do
+        bin when is_binary(bin) ->
+          bin
+
+        _ ->
+          :error
+      end
+    catch
+      _, _ -> :error
+    end
+  end
+
+  defp validate_otps([opt | rest], {:ok, acc}) do
+    validate_otps(rest, opt_valid?(opt, acc))
+  end
+
+  defp validate_otps([], {:ok, 2}), do: :ok
+  defp validate_otps(_, _acc), do: {:error, :enotsup}
+
+  defp opt_valid?(:binary, acc), do: {:ok, acc + 1}
+  defp opt_valid?({:binary, true}, acc), do: {:ok, acc + 1}
+  defp opt_valid?({:encoding, :unicode}, acc), do: {:ok, acc + 1}
+  defp opt_valid?(_opt, _acc), do: :error
 end

apps/elixir_ls_utils/test/output_device_test.exs
@@ -1,0 +1,329 @@
+defmodule ElixirLS.Utils.OutputDeviceTest do
+  use ExUnit.Case, async: false
+
+  alias ElixirLS.Utils.OutputDevice
+
+  defmodule FakeOutput do
+    use GenServer
+
+    def start_link(args) do
+      GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    end
+
+    def set_responses(responses) do
+      GenServer.call(__MODULE__, {:set_responses, responses})
+    end
+
+    def get_requests() do
+      GenServer.call(__MODULE__, :get_requests)
+    end
+
+    @impl GenServer
+    def init(_) do
+      {:ok, {[], []}}
+    end
+
+    @impl GenServer
+    def handle_call({:set_responses, responses}, _from, _state) do
+      {:reply, :ok, {[], responses}}
+    end
+
+    def handle_call(:get_requests, _from, state = {requests, _}) do
+      {:reply, requests |> Enum.reverse(), state}
+    end
+
+    @impl GenServer
+    def handle_info({:io_request, from, reply_as, req}, {requests, [resp | responses]}) do
+      send(from, {:io_reply, reply_as, resp})
+      {:noreply, {[req | requests], responses}}
+    end
+  end
+
+  defmodule FakeWireProtocol do
+    use GenServer
+
+    def start_link(args) do
+      GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    end
+
+    def send(msg) do
+      GenServer.call(__MODULE__, {:send, msg})
+    end
+
+    def get() do
+      GenServer.call(__MODULE__, :get)
+    end
+
+    @impl GenServer
+    def init(_), do: {:ok, []}
+
+    @impl GenServer
+    def handle_call({:send, msg}, _from, state) do
+      {:reply, :ok, [msg | state]}
+    end
+
+    def handle_call(:get, _from, state) do
+      {:reply, state |> Enum.reverse(), state}
+    end
+  end
+
+  setup do
+    {:ok, fake_user} = FakeOutput.start_link([])
+    {:ok, fake_wire_protocol} = FakeWireProtocol.start_link([])
+    {:ok, output_device} = OutputDevice.start_link(fake_user, &FakeWireProtocol.send/1)
+    {:ok, output_device_error} = OutputDevice.start_link(fake_user, fn _ -> {:error, :emfile} end)
+
+    {:ok,
+     %{
+       output_device: output_device,
+       output_device_error: output_device_error,
+       fake_wire_protocol: fake_wire_protocol,
+       fake_user: fake_user
+     }}
+  end
+
+  test "passes optional io_request to underlying device", %{
+    output_device: output_device
+  } do
+    FakeOutput.set_responses([{:ok, 77}, {:error, :enotsup}, :ok])
+
+    send(output_device, {:io_request, self(), 123, {:get_geometry, :rows}})
+    assert_receive({:io_reply, 123, {:ok, 77}})
+
+    send(output_device, {:io_request, self(), 123, {:get_geometry, :columns}})
+    assert_receive({:io_reply, 123, {:error, :enotsup}})
+
+    send(output_device, {:io_request, self(), 123, :some_unknown})
+    assert_receive({:io_reply, 123, :ok})
+
+    assert FakeOutput.get_requests() == [
+             {:get_geometry, :rows},
+             {:get_geometry, :columns},
+             :some_unknown
+           ]
+  end
+
+  describe "handles multi requests" do
+    test "all passed, all succeed, last response returned naked `:ok`", %{
+      output_device: output_device
+    } do
+      FakeOutput.set_responses([:ok, {:ok, ""}, :eof, :ok])
+
+      requests = [
+        :some,
+        :other1,
+        :other2,
+        :another
+      ]
+
+      send(output_device, {:io_request, self(), 123, {:requests, requests}})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeOutput.get_requests() == [:some, :other1, :other2, :another]
+    end
+
+    test "all passed, all succeed, last response returned wrapped", %{
+      output_device: output_device
+    } do
+      FakeOutput.set_responses([:ok, [:abc]])
+
+      requests = [
+        {:other, [:abc]},
+        :some
+      ]
+
+      send(output_device, {:io_request, self(), 123, {:requests, requests}})
+      assert_receive({:io_reply, 123, {:ok, [:abc]}})
+
+      assert FakeOutput.get_requests() == [{:other, [:abc]}, :some]
+    end
+
+    test "all passed, error breaks processing, last response returned", %{
+      output_device: output_device
+    } do
+      FakeOutput.set_responses([{:error, :notsup}])
+
+      requests = [
+        {:get_geometry, :rows},
+        :getopts
+      ]
+
+      send(output_device, {:io_request, self(), 123, {:requests, requests}})
+      assert_receive({:io_reply, 123, {:error, :notsup}})
+
+      assert FakeOutput.get_requests() == [{:get_geometry, :rows}]
+    end
+  end
+
+  def get_chars_list("abc"), do: 'some'
+  def get_chars_binary("abc"), do: "some"
+  def get_chars_invalid("abc"), do: :some
+  def get_chars_raise("abc"), do: raise(ArgumentError)
+  def get_chars_throw("abc"), do: throw(:foo)
+
+  describe "put_chars mfa" do
+    test "mfa list", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, __MODULE__, :get_chars_list, ["abc"]}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["some"]
+    end
+
+    test "mfa binary", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, __MODULE__, :get_chars_binary, ["abc"]}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["some"]
+    end
+
+    test "mfa invalid result", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, __MODULE__, :get_chars_invalid, ["abc"]}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, {:error, :put_chars}})
+
+      assert FakeWireProtocol.get() == []
+    end
+
+    test "mfa throw", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, __MODULE__, :get_chars_throw, ["abc"]}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, {:error, :put_chars}})
+
+      assert FakeWireProtocol.get() == []
+    end
+
+    test "mfa raise", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, __MODULE__, :get_chars_raise, ["abc"]}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, {:error, :put_chars}})
+
+      assert FakeWireProtocol.get() == []
+    end
+  end
+
+  describe "put_chars" do
+    test "returns error from output function", %{
+      output_device_error: output_device
+    } do
+      request = {:put_chars, :unicode, "sąme👨‍👩‍👦"}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, {:error, :emfile}})
+    end
+
+    test "unicode binary", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, "sąme👨‍👩‍👦"}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["sąme👨‍👩‍👦"]
+    end
+
+    test "unicode list", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :unicode, 'sąme👨‍👩‍👦'}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["sąme👨‍👩‍👦"]
+    end
+
+    test "latin1 binary", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :latin1, "some"}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["some"]
+    end
+
+    test "latin1 list", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :latin1, 'some'}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, :ok})
+
+      assert FakeWireProtocol.get() == ["some"]
+    end
+
+    test "latin1 list with chars > 255", %{
+      output_device: output_device
+    } do
+      request = {:put_chars, :latin1, 'sąme👨‍👩‍👦'}
+      send(output_device, {:io_request, self(), 123, request})
+      assert_receive({:io_reply, 123, {:error, :put_chars}})
+
+      assert FakeWireProtocol.get() == []
+    end
+  end
+
+  describe "opts" do
+    test "returns", %{
+      output_device: output_device
+    } do
+      send(output_device, {:io_request, self(), 123, :getopts})
+      assert_receive({:io_reply, 123, [binary: true, encoding: :unicode]})
+    end
+
+    test "valid can be set", %{
+      output_device: output_device
+    } do
+      send(
+        output_device,
+        {:io_request, self(), 123, {:setopts, [:binary, {:encoding, :unicode}]}}
+      )
+
+      assert_receive({:io_reply, 123, :ok})
+
+      send(
+        output_device,
+        {:io_request, self(), 123, {:setopts, [{:encoding, :unicode}, {:binary, true}]}}
+      )
+
+      assert_receive({:io_reply, 123, :ok})
+    end
+
+    test "rejects invalid", %{
+      output_device: output_device
+    } do
+      send(output_device, {:io_request, self(), 123, {:setopts, [:list, {:encoding, :unicode}]}})
+      assert_receive({:io_reply, 123, {:error, :enotsup}})
+
+      send(
+        output_device,
+        {:io_request, self(), 123, {:setopts, [{:encoding, :latin1}, {:binary, true}]}}
+      )
+
+      assert_receive({:io_reply, 123, {:error, :enotsup}})
+
+      send(
+        output_device,
+        {:io_request, self(), 123, {:setopts, [:binary, {:encoding, :unicode}, {:some, :value}]}}
+      )
+
+      assert_receive({:io_reply, 123, {:error, :enotsup}})
+
+      send(output_device, {:io_request, self(), 123, {:setopts, [:binary]}})
+      assert_receive({:io_reply, 123, {:error, :enotsup}})
+
+      send(output_device, {:io_request, self(), 123, {:setopts, [{:encoding, :unicode}]}})
+      assert_receive({:io_reply, 123, {:error, :enotsup}})
+    end
+  end
+end

apps/elixir_ls_debugger/lib/debugger/cli.ex
@@ -6,6 +6,7 @@ defmodule ElixirLS.Debugger.CLI do
     WireProtocol.intercept_output(&Output.print/1, &Output.print_err/1)
     Launch.start_mix()
     {:ok, _} = Application.ensure_all_started(:elixir_ls_debugger, :permanent)
+
     IO.puts("Started ElixirLS debugger v#{Launch.debugger_version()}")
     Launch.print_versions()
     Launch.limit_num_schedulers()

apps/elixir_ls_debugger/lib/debugger/output.ex
@@ -7,7 +7,7 @@ defmodule ElixirLS.Debugger.Output do
   are sent with sequence numbers that are unique and sequential, and includes client functions for
   sending these messages.
   """
-  import ElixirLS.Utils.WireProtocol, only: [send: 1]
+  alias ElixirLS.Utils.WireProtocol
   use GenServer
   use ElixirLS.Debugger.Protocol
 
@@ -29,12 +29,12 @@ defmodule ElixirLS.Debugger.Output do
     GenServer.call(server, {:send_event, event, body})
   end
 
-  def print(server \\ __MODULE__, str) do
-    send_event(server, "output", %{"category" => "stdout", "output" => to_string(str)})
+  def print(server \\ __MODULE__, str) when is_binary(str) do
+    send_event(server, "output", %{"category" => "stdout", "output" => str})
   end
 
-  def print_err(server \\ __MODULE__, str) do
-    send_event(server, "output", %{"category" => "stderr", "output" => to_string(str)})
+  def print_err(server \\ __MODULE__, str) when is_binary(str) do
+    send_event(server, "output", %{"category" => "stderr", "output" => str})
   end
 
   ## Server callbacks
@@ -46,27 +46,28 @@ defmodule ElixirLS.Debugger.Output do
 
   @impl GenServer
   def handle_call({:send_response, request_packet, body}, _from, seq) do
-    send(response(seq, request_packet["seq"], request_packet["command"], body))
-    {:reply, :ok, seq + 1}
+    res = WireProtocol.send(response(seq, request_packet["seq"], request_packet["command"], body))
+    {:reply, res, seq + 1}
   end
 
   def handle_call({:send_error_response, request_packet, message, format, variables}, _from, seq) do
-    send(
-      error_response(
-        seq,
-        request_packet["seq"],
-        request_packet["command"],
-        message,
-        format,
-        variables
+    res =
+      WireProtocol.send(
+        error_response(
+          seq,
+          request_packet["seq"],
+          request_packet["command"],
+          message,
+          format,
+          variables
+        )
       )
-    )
 
-    {:reply, :ok, seq + 1}
+    {:reply, res, seq + 1}
   end
 
   def handle_call({:send_event, event, body}, _from, seq) do
-    send(event(seq, event, body))
-    {:reply, :ok, seq + 1}
+    res = WireProtocol.send(event(seq, event, body))
+    {:reply, res, seq + 1}
   end
 end

apps/elixir_ls_debugger/lib/debugger/stacktrace.ex
@@ -6,7 +6,7 @@ defmodule ElixirLS.Debugger.Stacktrace do
   defmodule Frame do
     defstruct [:level, :file, :module, :function, :args, :line, :bindings]
 
-    def name(frame) do
+    def name(%__MODULE__{} = frame) do
       "#{inspect(frame.module)}.#{frame.function}/#{Enum.count(frame.args)}"
     end
   end
@@ -52,7 +52,7 @@ defmodule ElixirLS.Debugger.Stacktrace do
         [first_frame | other_frames]
 
       error ->
-        IO.warn("Failed to obtain meta pid for #{inspect(pid)}: #{error}")
+        IO.warn("Failed to obtain meta pid for #{inspect(pid)}: #{inspect(error)}")
         []
     end
   end

commit 18aedca916de5c94addcfd95849ceeb96d3d79d9

correctly encode output messages as binary
Fixes https://github.com/elixir-lsp/vscode-elixir-ls/issues/322
Bug introduced in https://github.com/elixir-lsp/elixir-ls/pull/457

apps/elixir_ls_utils/lib/output_device.ex
@@ -2,15 +2,9 @@ defmodule ElixirLS.Utils.OutputDevice do
   @moduledoc """
   Intercepts IO request messages and forwards them to the Output server to be sent as events to
   the IDE. Implements [Erlang I/O Protocol](https://erlang.org/doc/apps/stdlib/io_protocol.html)
-
-  In order to send console output to Visual Studio Code, the debug adapter needs to send events
-  using the usual wire protocol. In order to intercept the debugged code's output, we replace the
-  registered processes `:user` and `:standard_error` and the process's group leader with instances
-  of this server. When it receives a message containing output, it sends an event via the `Output`
-  server with the correct category ("stdout" or "stderr").
   """
 
-  @opts binary: true, encoding: :unicode
+  @opts binary: true, encoding: :latin1
 
   ## Client API
 

apps/elixir_ls_utils/test/output_device_test.exs
@@ -284,7 +284,7 @@ defmodule ElixirLS.Utils.OutputDeviceTest do
       output_device: output_device
     } do
       send(output_device, {:io_request, self(), 123, :getopts})
-      assert_receive({:io_reply, 123, [binary: true, encoding: :unicode]})
+      assert_receive({:io_reply, 123, [binary: true, encoding: :latin1]})
     end
 
     test "valid can be set", %{