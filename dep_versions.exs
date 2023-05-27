9184294b99cad8794fde607a512fd8e8e08969ce
Lukasz Samson <lukaszsamson@gmail.com>
2023-05-27 09:16:39 +0200

    update dialyxir to 1.3.0

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
   elixir_sense: "c9ccb3f093fe0fcb5bc155f90399bdbedb76018e",
-  dialyxir_vendored: "896fa45817c6a1be8ec408577c88ab52c27f6851",
+  dialyxir_vendored: "07c01786b5a9f1a409e44b90f3dfdc0650c5477f",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
