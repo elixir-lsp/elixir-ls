13a4cbb16099ea695aac43fbb45a9d72f7a30b8b
Lukasz Samson <lukaszsamson@gmail.com>
2023-05-28 08:06:58 +0200

    change dialyzer sha

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
   elixir_sense: "c9ccb3f093fe0fcb5bc155f90399bdbedb76018e",
-  dialyxir_vendored: "07c01786b5a9f1a409e44b90f3dfdc0650c5477f",
+  dialyxir_vendored: "7e908b4d760c7329046e0ee3076be9156cd784e1",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
