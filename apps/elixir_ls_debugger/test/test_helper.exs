79a493458c15accedba8a5049345211825e68d4a
Lukasz Samson <lukaszsamson@gmail.com>
2023-06-13 23:08:21 +0200

    add version check

--- apps/elixir_ls_debugger/test/test_helper.exs
+++ apps/elixir_ls_debugger/test/test_helper.exs
@@ -1,3 +1,7 @@
 Application.put_env(:elixir_ls_debugger, :test_mode, true)
 ExUnit.start(exclude: [pending: true])
-Mix.ensure_application!(:debugger)
+if Version.match?(System.version(), ">= 1.15.0-dev") do
+  # make sue that debugger modules are in code path
+  # without starting the app
+  Mix.ensure_application!(:debugger)
+end
