4a44e1f630ed231057f118799bebd6d760d07462
Lukasz Samson <lukaszsamson@gmail.com>
2023-06-19 22:26:00 +0200

    bump elixir_sense to latest

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "02960e4cd20eda70f9ace1d1c2bb9d17c093d3b3",
+  elixir_sense: "a6abc226d7ccbc386ffed02309d7379375150598",
   dialyxir_vendored: "7e908b4d760c7329046e0ee3076be9156cd784e1",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
