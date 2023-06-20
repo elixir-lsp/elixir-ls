d675398abadc40221a11f4270334f30cb8925c55
Lukasz Samson <lukaszsamson@gmail.com>
2023-06-20 08:13:54 +0200

    add 1.15 to CI matrix

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "a6abc226d7ccbc386ffed02309d7379375150598",
+  elixir_sense: "a50b8b616833fac6b45dbb65d6a8c6d9fda3a038",
   dialyxir_vendored: "7e908b4d760c7329046e0ee3076be9156cd784e1",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
