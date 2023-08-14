e1f2db44c079bccd9003017289bf5cb1fa963506
Lukasz Samson <lukaszsamson@gmail.com>
2023-08-14 12:17:03 +0200

    debug deadlock

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "1d701ea30b5f470d2a2c0c3c9727ba1b6c8fab66",
+  elixir_sense: "52ee95beaf21cc03075333562cc3c7cb75e9a6d8",
   dialyxir_vendored: "7e908b4d760c7329046e0ee3076be9156cd784e1",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
