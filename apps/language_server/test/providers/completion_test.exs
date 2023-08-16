9738bece24389c6b26d2bd803f79bc5cb2dfaa31
Lukasz Samson <lukaszsamson@gmail.com>
2023-08-16 07:33:13 +0200

    make completion tests sync

--- apps/language_server/test/providers/completion_test.exs
+++ apps/language_server/test/providers/completion_test.exs
@@ -1,5 +1,5 @@
 defmodule ElixirLS.LanguageServer.Providers.CompletionTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
   require Logger
 
--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "52ee95beaf21cc03075333562cc3c7cb75e9a6d8",
+  elixir_sense: "27df8842a2582ab7bf114347eb2e1cad77b342a3",
   dialyxir_vendored: "7e908b4d760c7329046e0ee3076be9156cd784e1",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa"
 ]
