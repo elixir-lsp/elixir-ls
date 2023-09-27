1dc1e1bddc0cba2e953626ae4d277185a521dd32
Lukasz Samson <lukaszsamson@gmail.com>
2023-09-27 23:08:48 +0200

    Mix.Dep.load_on_environment no longer exists on 1.16

--- apps/language_server/lib/language_server/build.ex
+++ apps/language_server/lib/language_server/build.ex
@@ -20,7 +20,11 @@ defmodule ElixirLS.LanguageServer.Build do
                   with_diagnostics([log: true], fn ->
                     try do
                       # this call can raise
-                      current_deps = Mix.Dep.load_on_environment([])
+                      current_deps = if Version.match?(System.version(), "< 1.16-dev") do
+                      Mix.Dep.load_on_environment([])
+                    else
+                      Mix.Dep.Converger.converge([])
+                    end
 
                       purge_changed_deps(current_deps, cached_deps)
 
