529162ef80429312228f268a345dd8a8d894d3ec
Lukasz Samson <lukaszsamson@gmail.com>
2023-10-10 21:34:55 +0200

    optimise app retrieval

--- apps/language_server/lib/language_server/doc_links.ex
+++ apps/language_server/lib/language_server/doc_links.ex
@@ -5,27 +5,14 @@ defmodule ElixirLS.LanguageServer.DocLinks do
 
   @hex_base_url "https://hexdocs.pm"
 
-  defp get_erts_modules do
-    {:ok, [[erlang_lib_dir]]} = :init.get_argument(:root)
-    erts_version = :erlang.system_info(:version)
-    erts_app_path = Path.join([erlang_lib_dir, "lib", "erts-#{erts_version}", "ebin", "erts.app"])
-
-    {:ok, [{:application, _, props}]} = :file.consult(erts_app_path)
-    modules = Keyword.get(props, :modules)
-    for module <- modules, into: %{}, do: {module, {:erts, erts_version}}
-  end
-
   defp get_app(module) do
-    module_to_app =
-      for {app, _, vsn} <- Application.loaded_applications(),
-          {:ok, app_modules} = :application.get_key(app, :modules),
-          mod <- app_modules,
-          into: %{},
-          do: {mod, {app, vsn}}
-
-    module_to_app
-    |> Map.merge(get_erts_modules())
-    |> Map.get(module)
+    with {:ok, app} <- :application.get_application(module),
+         {:ok, vsn} <- :application.get_key(app, :vsn) do
+      {app, vsn}
+    else
+      _ ->
+        nil
+    end
   end
 
   def hex_docs_module_link(module) do
