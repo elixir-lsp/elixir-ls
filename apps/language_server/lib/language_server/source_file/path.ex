60371a7703a515c76a6a356c4196713c137895c2
Lukasz Samson <lukaszsamson@gmail.com>
2023-11-25 22:25:57 +0100

    fix dialyzer error

--- apps/language_server/lib/language_server/source_file/path.ex
+++ apps/language_server/lib/language_server/source_file/path.ex
@@ -205,7 +205,7 @@ defmodule ElixirLS.LanguageServer.SourceFile.Path do
     absname(path, &File.cwd!/0)
   end
 
-  @spec absname(t, t) :: binary
+  @spec absname(t, t | (-> t)) :: binary
   def absname(path, relative_to) do
     path = IO.chardata_to_string(path)
 
