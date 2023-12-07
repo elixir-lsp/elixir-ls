759d837b13cd1e72735d6f995cbc21120c53d837
Lukasz Samson <lukaszsamson@gmail.com>
2023-12-07 22:28:25 +0100

    fix tests

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "3c8ae4e97c70a4d369e0abe1ba39d8bdaa9d1de3",
+  elixir_sense: "62b777815e9bd8eca7767c0598c8be8384c1c5ee",
   dialyxir_vendored: "d50dcd7101c6ebd37b57b7ee4a7888d8cb634782",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
