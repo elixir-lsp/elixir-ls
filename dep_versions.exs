ddf6dca19e5d4189325c4e75f5f8f5674975af31
Lukasz Samson <lukaszsamson@gmail.com>
2024-01-21 17:11:52 +0100

    bump elixir_sense

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "06ed2cc393f7d773b8c70fa48767558052ff8962",
+  elixir_sense: "e4c0282b8c2afa68652227992a223b1a9aab2c54",
   dialyxir_vendored: "d50dcd7101c6ebd37b57b7ee4a7888d8cb634782",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
