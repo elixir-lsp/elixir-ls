585cdb6badf38e288bf636d1b190b78cd2eec431
Lukasz Samson <lukaszsamson@gmail.com>
2024-03-10 09:06:05 +0100

    add copyright notices

--- apps/elixir_ls_utils/lib/completion_engine.ex
+++ apps/elixir_ls_utils/lib/completion_engine.ex
@@ -1,3 +1,9 @@
+# This code has originally been a part of https://github.com/elixir-lsp/elixir_sense
+
+# Copyright (c) 2017 Marlus Saraiva
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 # This file includes modified code extracted from the elixir project. Namely:
 #
 # https://github.com/elixir-lang/elixir/blob/v1.1/lib/iex/lib/iex/autocomplete.exs
