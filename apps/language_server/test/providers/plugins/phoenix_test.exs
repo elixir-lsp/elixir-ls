b7392befea3aabe940fef7c2b949d2bd1131758f
Lukasz Samson <lukaszsamson@gmail.com>
2024-05-04 07:32:07 +0200

    skip tests on < 1.14

--- apps/language_server/test/providers/plugins/phoenix_test.exs
+++ apps/language_server/test/providers/plugins/phoenix_test.exs
@@ -28,129 +28,130 @@ defmodule ElixirLS.LanguageServer.Plugins.PhoenixTest do
     )
   end
 
-  @moduletag requires_elixir_1_14: true
-  describe "suggestions/4" do
-    test "overrides with controllers for phoenix_route_funcs, when in the second parameter" do
-      buffer = """
-      defmodule ExampleWeb.Router do
-        import Phoenix.Router
+  if Version.match?(System.version(), ">= 1.14.0") do
+    describe "suggestions/4" do
+      test "overrides with controllers for phoenix_route_funcs, when in the second parameter" do
+        buffer = """
+        defmodule ExampleWeb.Router do
+          import Phoenix.Router
 
-        get "/", P
-        #         ^
+          get "/", P
+          #         ^
+        end
+        """
+
+        [cursor] = cursors(buffer)
+
+        result = suggestions(buffer, cursor)
+
+        assert [
+                 %{
+                   type: :generic,
+                   kind: :class,
+                   label: "ExampleWeb.PageController",
+                   insert_text: "ExampleWeb.PageController",
+                   detail: "Phoenix controller"
+                 }
+               ] = result
       end
-      """
 
-      [cursor] = cursors(buffer)
+      test "do not prepend alias defined within Phoenix `scope` functions" do
+        _define_existing_atom = ExampleWeb
 
-      result = suggestions(buffer, cursor)
+        buffer = """
+          defmodule ExampleWeb.Router do
+            import Phoenix.Router
 
-      assert [
-               %{
-                 type: :generic,
-                 kind: :class,
-                 label: "ExampleWeb.PageController",
-                 insert_text: "ExampleWeb.PageController",
-                 detail: "Phoenix controller"
-               }
-             ] = result
-    end
+            scope "/", ExampleWeb do
+              get "/", P
+              #         ^
+            end
+          end
+        """
 
-    test "do not prepend alias defined within Phoenix `scope` functions" do
-      _define_existing_atom = ExampleWeb
+        [cursor] = cursors(buffer)
 
-      buffer = """
+        result = suggestions(buffer, cursor)
+
+        assert [
+                 %{
+                   type: :generic,
+                   kind: :class,
+                   label: "ExampleWeb.PageController",
+                   insert_text: "PageController",
+                   detail: "Phoenix controller"
+                 }
+               ] = result
+      end
+
+      test "overrides with action suggestions for phoenix_route_funcs, when in the third parameter" do
+        buffer = """
+        defmodule ExampleWeb.Router do
+          import Phoenix.Router
+
+          get "/", ExampleWeb.PageController, :
+          #                                    ^
+        end
+        """
+
+        [cursor] = cursors(buffer)
+
+        result = suggestions(buffer, cursor)
+
+        assert [
+                 %{
+                   detail: "Phoenix action",
+                   insert_text: "home",
+                   kind: :function,
+                   label: ":home",
+                   type: :generic
+                 }
+               ] = result
+      end
+
+      test "overrides with action suggestions even when inside scope" do
+        buffer = """
         defmodule ExampleWeb.Router do
           import Phoenix.Router
 
           scope "/", ExampleWeb do
-            get "/", P
-            #         ^
+            get "/", PageController, :
+            #                         ^
           end
         end
-      """
+        """
 
-      [cursor] = cursors(buffer)
+        [cursor] = cursors(buffer)
 
-      result = suggestions(buffer, cursor)
+        result = suggestions(buffer, cursor)
 
-      assert [
-               %{
-                 type: :generic,
-                 kind: :class,
-                 label: "ExampleWeb.PageController",
-                 insert_text: "PageController",
-                 detail: "Phoenix controller"
-               }
-             ] = result
-    end
-
-    test "overrides with action suggestions for phoenix_route_funcs, when in the third parameter" do
-      buffer = """
-      defmodule ExampleWeb.Router do
-        import Phoenix.Router
-
-        get "/", ExampleWeb.PageController, :
-        #                                    ^
+        assert [
+                 %{
+                   detail: "Phoenix action",
+                   insert_text: "home",
+                   kind: :function,
+                   label: ":home",
+                   type: :generic
+                 }
+               ] = result
       end
-      """
 
-      [cursor] = cursors(buffer)
+      test "ignores for non-phoenix_route_funcs" do
+        buffer = """
+        defmodule ExampleWeb.Router do
+          import Phoenix.Router
 
-      result = suggestions(buffer, cursor)
-
-      assert [
-               %{
-                 detail: "Phoenix action",
-                 insert_text: "home",
-                 kind: :function,
-                 label: ":home",
-                 type: :generic
-               }
-             ] = result
-    end
-
-    test "overrides with action suggestions even when inside scope" do
-      buffer = """
-      defmodule ExampleWeb.Router do
-        import Phoenix.Router
-
-        scope "/", ExampleWeb do
-          get "/", PageController, :
-          #                         ^
+          something_else "/", P
+          #                    ^
         end
+        """
+
+        [cursor] = cursors(buffer)
+
+        result = suggestions(buffer, cursor)
+
+        refute Enum.find(result, &(&1[:detail] == "Phoenix controller"))
       end
-      """
-
-      [cursor] = cursors(buffer)
-
-      result = suggestions(buffer, cursor)
-
-      assert [
-               %{
-                 detail: "Phoenix action",
-                 insert_text: "home",
-                 kind: :function,
-                 label: ":home",
-                 type: :generic
-               }
-             ] = result
-    end
-
-    test "ignores for non-phoenix_route_funcs" do
-      buffer = """
-      defmodule ExampleWeb.Router do
-        import Phoenix.Router
-
-        something_else "/", P
-        #                    ^
-      end
-      """
-
-      [cursor] = cursors(buffer)
-
-      result = suggestions(buffer, cursor)
-
-      refute Enum.find(result, &(&1[:detail] == "Phoenix controller"))
     end
   end
 end
