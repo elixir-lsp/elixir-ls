1f1f88b9a5d96569f821d9b189ee052745466e64
Lukasz Samson <lukaszsamson@gmail.com>
2024-06-02 21:22:38 +0200

    bump dialyxir

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,6 +1,6 @@
 [
   elixir_sense: "314e77f7914aeb38e2d8d150582174c579795b9f",
-  dialyxir_vendored: "d50dcd7101c6ebd37b57b7ee4a7888d8cb634782",
+  dialyxir_vendored: "462e599aa7004a32cfa548cc715c9c59e95dacaf",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
   path_glob_vendored: "965350dc41def7be4a70a23904195c733a2ecc84"
