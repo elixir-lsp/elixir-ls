98c428958fd5f7dabf632c056cfa2ccef33b7027
Lukasz Samson <lukaszsamson@gmail.com>
2024-08-20 08:06:40 +0200

    switch to elixir_sense expand branch

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "4adfc55dc6902f56e6d070e14fba768a0ff05bd3",
+  elixir_sense: "88e3e432f8891687bfe7ad21a2d6e2ddd0e5cef2",
   dialyxir_vendored: "462e599aa7004a32cfa548cc715c9c59e95dacaf",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
