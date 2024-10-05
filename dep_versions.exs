b09a91d27dd74a839f413bd7c0d4f166609128a0
Lukasz Samson <lukaszsamson@gmail.com>
2024-10-05 12:32:06 +0200

    update dialyxir and erlex

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,6 +1,6 @@
 [
-  elixir_sense: "f2b6172e9f7d8da2a1039888e355cc7b7757c2b5",
-  dialyxir_vendored: "462e599aa7004a32cfa548cc715c9c59e95dacaf",
+  elixir_sense: "1a23c9c570c7e2344728aaedf9a79204601672d8",
+  dialyxir_vendored: "f8f64cfb6797c518294687e7c03ae817bacbc6ee",
   jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
   path_glob_vendored: "965350dc41def7be4a70a23904195c733a2ecc84"
