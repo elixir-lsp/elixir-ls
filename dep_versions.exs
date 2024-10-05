189b477d6f644f811daf1ece4aaa7add53718086
Lukasz Samson <lukaszsamson@gmail.com>
2024-10-05 16:52:38 +0200

    update jason

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,7 +1,7 @@
 [
   elixir_sense: "1a23c9c570c7e2344728aaedf9a79204601672d8",
   dialyxir_vendored: "f8f64cfb6797c518294687e7c03ae817bacbc6ee",
-  jason_v: "c81537e2a5e1acacb915cf339fe400357e3c2aaa",
+  jason_v: "f1c10fa9c445cb9f300266122ef18671054b2330",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
   path_glob_vendored: "965350dc41def7be4a70a23904195c733a2ecc84"
 ]
