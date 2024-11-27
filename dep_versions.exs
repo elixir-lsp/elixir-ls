e2d01f3298194285f45d556f9e3a72b3d5b3053c
Denny Mueller <contact@dennymueller.io>
2024-11-27 22:34:34 +0100

    bump elixir_sense to latest commit

--- dep_versions.exs
+++ dep_versions.exs
@@ -1,5 +1,5 @@
 [
-  elixir_sense: "6c293f27ea3afc4f5913a7ed03a07db0fa15a0a2",
+  elixir_sense: "67f6974dedb33846a060031d5afd5430a3f583f0",
   dialyxir_vendored: "f8f64cfb6797c518294687e7c03ae817bacbc6ee",
   jason_v: "f1c10fa9c445cb9f300266122ef18671054b2330",
   erl2ex_vendored: "073ac6b9a44282e718b6050c7b27cedf9217a12a",
